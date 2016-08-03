5831a0659cb0f68c1a5fba2fe966433b147280ea
ManaPHP Mark <manaphp@qq.com>
2016-08-03 23:36:24 +0800

    Added      nginx.sample Modified   server.php Modified   ManaPHP/manaphp.dic

--- server.php
+++ server.php
@@ -1,27 +1,28 @@
 <?php
 
-$_SERVER['SERVER_ADDR']='localhost';
-$_SERVER['REQUEST_SCHEME']='http';
-if(PHP_SAPI ==='cli'){
-	$bind=$_SERVER['SERVER_ADDR'].':83';
-	echo 'server listen on: '.$bind;
+$_SERVER['SERVER_ADDR'] = 'localhost';
+$_SERVER['REQUEST_SCHEME'] = 'http';
+if (PHP_SAPI === 'cli') {
+    $bind = $_SERVER['SERVER_ADDR'] . ':83';
+    echo 'server listen on: ' . $bind, PHP_EOL;
 
-	`explorer.exe "$_SERVER[REQUEST_SCHEME]://$bind/"`;
-	`php -S $bind server.php`;
-	return 0;
+    if (DIRECTORY_SEPARATOR === '\\') {
+        $r = `explorer.exe $_SERVER[REQUEST_SCHEME]://$bind/`;
+    }
+
+    chdir(__DIR__);
+
+    $r = `php -S $bind -t Public server.php`;
+    return $r;
 }
-
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a ManaPHP
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/Public'.$uri)) {
+if ($uri !== '/' && file_exists(__DIR__ . '/Public' . $uri)) {
     return false;
 }
-$_GET['_url']=$uri;
-require_once __DIR__.'/Public/index.php';
+
+$_GET['_url'] = $uri;
+require_once __DIR__ . '/Public/index.php';
