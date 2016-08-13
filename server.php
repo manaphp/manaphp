6583f0177b0209dac22e239df575370bfce4db43
ManaPHP Mark <manaphp@qq.com>
2016-08-13 10:27:51 +0800

    Modified   ManaPHP/Mvc/View/Renderer/Engine/Sword.php Modified   Application/Home/Views/Index/About.sword Modified   Public/.htaccess Modified   server.php

--- server.php
+++ server.php
@@ -21,8 +21,12 @@ $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a ManaPHP
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__ . '/Public' . $uri)) {
-    return false;
+if ($uri !== '/') {
+    if (file_exists(__DIR__ . '/Public/' . $uri)
+        || preg_match('#(.css|.js|.gif|.png|.jpg|.jpeg|.ttf|.woff|.ico)$#', $uri) === 1
+    ) {
+        return false;
+    }
 }
 
 $_GET['_url'] = $uri;
