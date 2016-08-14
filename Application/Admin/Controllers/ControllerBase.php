27752c2b14f83da1859bf097d0a023b218128e1c
ManaPHP Mark <manaphp@qq.com>
2016-08-14 22:01:09 +0800

    Deleted    Application/Admin/Views/Layouts/RbacPermission.sword Added      Application/Admin/Views/RbacPermission/Header.sword Added      Public/AdminLTE/css/AdminLTE.css Added      Public/AdminLTE/css/AdminLTE.min.css Added      Public/AdminLTE/css/skins/skin-blue.css Added      Public/AdminLTE/css/skins/skin-blue.min.css Added      Public/AdminLTE/img/avatar.jpg Added      Public/AdminLTE/js/app.js Added      Public/AdminLTE/js/app.min.js Added      Public/favicon.ico Modified   Application/Admin/Controllers/ControllerBase.php Modified   Application/Admin/Controllers/RbacPermissionController.php Modified   Application/Admin/Views/Layouts/Default.sword Modified   Application/Admin/Views/RbacPermission/Index.sword Modified   Application/Admin/Views/Shared/Footer.sword Modified   Application/Admin/Views/Shared/Header.sword

--- Application/Admin/Controllers/ControllerBase.php
+++ Application/Admin/Controllers/ControllerBase.php
@@ -2,8 +2,12 @@
 namespace Application\Admin\Controllers;
 
 use ManaPHP\Mvc\Controller;
+use ManaPHP\Mvc\DispatcherInterface;
 
 class ControllerBase extends Controller
 {
-
+    public function beforeExecuteRoute(DispatcherInterface $dispatcher)
+    {
+        $this->view->setLayout('Default');
+    }
 }
--- Application/Admin/Controllers/RbacPermissionController.php
+++ Application/Admin/Controllers/RbacPermissionController.php
@@ -1,16 +1,62 @@
 <?php
 namespace Application\Admin\Controllers;
 
+use ManaPHP\Authorization\Rbac\Annotation;
 use ManaPHP\Authorization\Rbac\Models\Permission;
 use ManaPHP\Mvc\Controller;
 
-class RbacPermissionController extends Controller{
-    public function indexAction(){
-        $items=$this->modelsManager->createBuilder()
-                ->columns('*')
-                ->addFrom(Permission::class)
-                ->execute();
+class RbacPermissionController extends Controller
+{
+    public function indexAction()
+    {
+        $this->view->setLayout();
+        $items = $this->modelsManager->createBuilder()
+            ->columns('*')
+            ->addFrom(Permission::class)
+            ->execute();
 
-        $this->view->setVar('items',$items);
+        $data = ['permissions' => $items];
+        $this->view->setVar('data', $data);
+    }
+
+    public function rebuildAction()
+    {
+        $annotation = new Annotation();
+        foreach (glob($this->alias->get('@app') . '/*') as $entry) {
+            if (!is_dir($entry)) {
+                continue;
+            }
+
+            if ($entry === '.' || $entry === '..') {
+                continue;
+            }
+
+            $files = glob($entry . '/Controllers/*Controller.php');
+            foreach ($files as $file) {
+                $file = str_replace('\\', '/', $file);
+
+                if (preg_match('#([^/]*/[^/]*/Controllers/.*Controller)\.php$#', $file, $match) === 1) {
+                    $controller = str_replace('/', '\\', $match[1]);
+                    $permissions = $annotation->getPermissions($controller);
+                    foreach ($permissions as $p) {
+                        $permission = Permission::findFirst(['module' => $p['module'], 'controller' => $p['controller'], 'action' => $p['action']]);
+                        if ($permission === false) {
+                            $permission = new Permission();
+
+                            $permission->module = $p['module'];
+                            $permission->controller = $p['controller'];
+                            $permission->action = $p['action'];
+                            $permission->description = $p['description'];
+                            $permission->created_time = time();
+                            $permission->permission_type = Permission::TYPE_PENDING;
+
+                            $permission->create();
+                        }
+                    }
+                }
+            }
+        }
+
+        return $this->response->redirect('/admin/rbac_permission');
     }
 }
