70e69c8a80ff609e4a4898e39000e320098282f5
ManaPHP Mark <manaphp@qq.com>
2016-09-14 23:55:45 +0800

    Added      ManaPHP/Security/Random.php Added      unit-tests/SecurityRandomTest.php

---
+++ ManaPHP/Security/Random.php
@@ -0,0 +1,89 @@
+<?php
+namespace ManaPHP\Security;
+
+class Random
+{
+    /**
+     * @param int $length
+     *
+     * @return string
+     */
+    public function getByte($length)
+    {
+        if (function_exists('random_bytes')) {
+            return random_bytes($length);
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            return openssl_random_pseudo_bytes($length);
+        } elseif (file_exists('/dev/urandom')) {
+            $handle = fopen('/dev/urandom', 'rb');
+
+            stream_set_read_buffer($handle, 0);
+            $r = fread($handle, $length);
+            fclose($handle);
+
+            return $r;
+        } else {
+            $r = '';
+
+            do {
+                $r .= pack('L', mt_rand());
+            } while (strlen($r) < $length);
+
+            return substr($r, 0, $length);
+        }
+    }
+
+    /**
+     * @param int $length
+     * @param int $base
+     *
+     * @return string
+     */
+    public function getBase($length, $base = 62)
+    {
+        $str = '';
+
+        $bytes = $this->getByte($length);
+        for ($i = 0; $i < $length; $i++) {
+            $r = ord($bytes[$i]) % $base;
+
+            if ($r < 10) {
+                $str .= chr(ord('0') + $r);
+            } elseif ($r < 36) {
+                $str .= chr(ord('a') + $r - 10);
+            } else {
+                $str .= chr(ord('A') + $r - 36);
+            }
+        }
+
+        return $str;
+    }
+
+    /**
+     * @param int $min
+     * @param int $max
+     *
+     * @return int
+     */
+    public function getInt($min = 0, $max = 4294967296)
+    {
+        if ($min == $max) {
+            return $min;
+        } else {
+            return $min + $this->getByte(4) % ($max - $min);
+        }
+    }
+
+    /**
+     * @param float $min
+     * @param float $max
+     *
+     * @return float
+     */
+    public function getFloat($min = 0.0, $max = 1.0)
+    {
+        return $min + $this->getByte(4) / 4294967296 * ($max - $min);
+    }
+
+
+}
---
+++ unit-tests/SecurityRandomTest.php
@@ -0,0 +1,35 @@
+<?php
+defined('UNIT_TESTS_ROOT') || require __DIR__ . '/bootstrap.php';
+
+class SecurityRandomTest extends TestCase
+{
+    public function test_getByte()
+    {
+        $random = new \ManaPHP\Security\Random();
+        $this->assertEquals(0, strlen($random->getByte(0)));
+        $this->assertEquals(1, strlen($random->getByte(1)));
+        $this->assertEquals(1024, strlen($random->getByte(1024)));
+    }
+
+    public function test_getBase62()
+    {
+        $random = new \ManaPHP\Security\Random();
+        $this->assertEquals(0, strlen($random->getBase(0)));
+        $this->assertEquals(1, strlen($random->getBase(1)));
+        $this->assertEquals(1024, strlen($random->getBase(1024)));
+    }
+
+    public function test_getInt()
+    {
+        $random = new \ManaPHP\Security\Random();
+        $this->assertEquals(1, $random->getInt(1));
+    }
+
+    public function test_getFloat()
+    {
+        $random = new \ManaPHP\Security\Random();
+        $this->assertEquals(0.1, $random->getFloat(0.1, 0.1));
+    }
+
+
+}
