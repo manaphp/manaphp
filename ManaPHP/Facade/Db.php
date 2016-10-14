55e9fc4c95009dfd930327b2060fcdbb7f50a221
ManaPHP Mark <manaphp@qq.com>
2016-10-14 22:11:11 +0800

    Added      ManaPHP/Facade/Db.php

---
+++ ManaPHP/Facade/Db.php
@@ -0,0 +1,17 @@
+<?php
+namespace ManaPHP\Facade;
+
+use ManaPHP\Facade;
+
+/** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
+
+/**
+ * Class Request
+ *
+ * @package ManaPHP\Facade
+ * @method  static \ManaPHP\DbInterface getFacadeInstance()
+ */
+class Db extends Facade
+{
+
+}
