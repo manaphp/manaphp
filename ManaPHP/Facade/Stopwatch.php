c21c71962fb193fa6ecb11afe4c162364ffc1890
ManaPHP Mark <manaphp@qq.com>
2016-12-25 19:08:14 +0800

    Added      ManaPHP/Facade/Stopwatch.php

---
+++ ManaPHP/Facade/Stopwatch.php
@@ -0,0 +1,17 @@
+<?php
+namespace ManaPHP\Facade;
+
+use ManaPHP\Facade;
+use ManaPHP\StopwatchInterface;
+
+/**
+ * Class ManaPHP\Facade\Stopwatch
+ *
+ * @package tasksMetadata
+ *
+ * @method  static StopwatchInterface getFacadeInstance()
+ */
+class Stopwatch extends Facade
+{
+
+}
