81f1c9d94fe136ee6ec8679dd8ffa52da62915fa
ManaPHP Mark <manaphp@qq.com>
2017-07-02 10:50:08 +0800

    Deleted    Application/Cli/Controllers/MinifyController.php Modified   ManaPHP/Cli/Controllers/FrameworkController.php

--- ManaPHP/Cli/Controllers/FrameworkController.php
+++ ManaPHP/Cli/Controllers/FrameworkController.php
@@ -3,6 +3,7 @@
 namespace ManaPHP\Cli\Controllers;
 
 use ManaPHP\Cli\Controller;
+use ManaPHP\Utility\Text;
 
 class FrameworkController extends Controller
 {
@@ -33,9 +34,8 @@ class FrameworkController extends Controller
         }
 
         $contents = '';
-        $classes = $config['classes'];
 
-        foreach ($classes as $c) {
+        foreach ($config['classes'] as $c) {
             if (strpos($c, 'ManaPHP\\') !== 0) {
                 continue;
             }
@@ -86,5 +86,89 @@ class FrameworkController extends Controller
     public function __destruct()
     {
         $this->filesystem->fileDelete($this->_tmpLiteFile);
+    }
+
+    protected function _getSourceFiles($dir)
+    {
+        $files = [];
+
+        $dh = opendir($dir);
+        while ($file = readdir($dh)) {
+            if (Text::startsWith($file, '.')) {
+                continue;
+            }
+
+            $file = str_replace('\\', '/', $dir) . '/' . $file;
+            if (is_dir($file)) {
+                /** @noinspection SlowArrayOperationsInLoopInspection */
+                $files = array_merge($files, $this->_getSourceFiles($file));
+            } else {
+                if (fnmatch('*.php', $file)) {
+                    $files[] = $file;
+                }
+            }
+        }
+
+        closedir($dh);
+
+        return $files;
+    }
+
+    protected function _minify($content)
+    {
+        $content = preg_replace('#\s*/\*\*.*?\*/#ms', '', $content);//remove comments
+        $content = preg_replace('#([\r\n]+)\s*\\1#', '\\1', $content);//remove blank lines
+        $content = preg_replace('#([\r\n]+)\s+{#', '{', $content);//repositionClose;
+
+        return $content;
+    }
+
+    /**
+     * @CliCommand minify framework source code
+     * @return int
+     */
+    public function minifyCommand()
+    {
+        $ManaPHPSrcDir = $this->alias->get('@manaphp');
+        $ManaPHPDstDir = $ManaPHPSrcDir . '_' . date('ymd');
+        $totalClassLines = 0;
+        $totalInterfaceLines = 0;
+        $totalLines = 0;
+        $fileLines = [];
+        $sourceFiles = $this->_getSourceFiles($ManaPHPSrcDir);
+        foreach ($sourceFiles as $file) {
+            $dstFile = str_replace($ManaPHPSrcDir, $ManaPHPDstDir, $file);
+
+            $content = $this->_minify($this->filesystem->fileGet($file));
+            $lineCount = Text::contains($content, "\r") ? substr_count($content, "\r") : substr_count($content, "\n");
+
+            if (Text::contains($file, 'Interface.php')) {
+                $totalInterfaceLines += $lineCount;
+                $totalLines += $lineCount;
+            } else {
+                $totalClassLines += $lineCount;
+                $totalLines += $lineCount;
+            }
+
+            $this->console->writeLn($content);
+            $this->filesystem->filePut($dstFile, $content);
+            $fileLines[$file] = $lineCount;
+        }
+
+        asort($fileLines);
+
+        $i = 1;
+        $this->console->writeLn('------------------------------------------------------');
+
+        foreach ($fileLines as $file => $line) {
+            $this->console->writeLn(sprintf('%3d %3d %.3f', $i++, $line, $line / $totalLines * 100) . ' ' . substr($file, strpos($file, 'ManaPHP')));
+        }
+
+        $this->console->writeLn('------------------------------------------------------');
+        $this->console->writeLn('total     lines: ' . $totalLines);
+        $this->console->writeLn('class     lines: ' . $totalClassLines);
+        $this->console->writeLn('interface lines:  ' . $totalInterfaceLines);
+
+        return 0;
     }
 }
