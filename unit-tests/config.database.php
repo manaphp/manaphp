19524900774183987d3ee73b9ee975d724c1ae50
ManaPHP Mark <manaphp@qq.com>
2017-08-19 13:14:16 +0800

    Modified   unit-tests/config.database.php

--- unit-tests/config.database.php
+++ unit-tests/config.database.php
@@ -1,12 +1,6 @@
 <?php
 return [
-    'mysql' => [
-        'host' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'dbname' => 'manaphp_unit_test',
-    ],
-
+    'mysql' => 'mysql://root@localhost:/manaphp_unit_test',
     'sqlite' => [
         'file' => 'd:/wamp/manaphp_unit_test.sqlite'
     ]
