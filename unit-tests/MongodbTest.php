e31e14bc76e05448f1917a83a4f49f45a5bd6d78
ManaPHP Mark <manaphp@qq.com>
2017-08-25 21:51:43 +0800

    Add        unit-tests/MongodbTest.php

---
+++ unit-tests/MongodbTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 2015/12/23
+ * Time: 21:36
+ */
+defined('UNIT_TESTS_ROOT') || require __DIR__ . '/bootstrap.php';
+
+class MongodbTest extends TestCase
+{
+    /**
+     * @var \ManaPHP\MongodbInterface
+     */
+    protected $mongodb;
+
+    public function setUp()
+    {
+        new \ManaPHP\Di\FactoryDefault();
+
+        $config = require __DIR__ . '/config.database.php';
+        //$this->db = new ManaPHP\Db\Adapter\Mysql($config['mysql']);
+
+        $this->mongodb = new ManaPHP\Mongodb($config['mongodb']);
+    }
+
+    public function test_query()
+    {
+        //general usage
+        $documents = $this->mongodb->query('city', [], ['limit' => 3]);
+        $this->assertCount(3, $documents);
+    }
+
+}
