7fa74ed551304a0bd6892150f45a6d353d118e44
ManaPHP Mark <manaphp@qq.com>
2017-08-25 21:44:28 +0800

    Modified   unit-tests/config.database.php

--- unit-tests/config.database.php
+++ unit-tests/config.database.php
@@ -3,5 +3,6 @@ return [
     'mysql' => 'mysql://root@localhost:/manaphp_unit_test',
     'sqlite' => [
         'file' => 'd:/wamp/manaphp_unit_test.sqlite'
-    ]
+    ],
+    'mongodb' => 'mongodb://localhost:27017/manaphp_unit_test',
 ];
