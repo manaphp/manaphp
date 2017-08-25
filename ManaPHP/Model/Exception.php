0d6fcbfd89d1e485bc93809dabe62e4f16ddd2bd
ManaPHP Mark <manaphp@qq.com>
2017-08-25 20:23:53 +0800

    Added      ManaPHP/Model.php Added      ManaPHP/Model/Exception.php Added      ManaPHP/ModelInterface.php Modified   ManaPHP/Db/Model.php Modified   ManaPHP/Db/ModelInterface.php

---
+++ ManaPHP/Model/Exception.php
@@ -0,0 +1,7 @@
+<?php
+namespace ManaPHP\Model;
+
+class Exception extends \ManaPHP\Exception
+{
+
+}
---
+++ ManaPHP/ModelInterface.php
@@ -0,0 +1,280 @@
+<?php
+namespace ManaPHP;
+
+interface ModelInterface
+{
+    /**
+     * Returns table name mapped in the model
+     * <code>
+     *  $city->getSource();
+     * </code>
+     *
+     * @param mixed $context
+     *
+     * @return string|false
+     */
+    public static function getSource($context = null);
+
+    /**
+     * Gets internal database connection
+     *
+     * @param mixed $context
+     *
+     * @return string|false
+     */
+    public static function getDb($context = null);
+
+    /**
+     * @return array
+     */
+    public static function getPrimaryKey();
+
+    /**
+     * @return array
+     */
+    public static function getFields();
+
+    /**
+     * @return string
+     */
+    public static function getAutoIncrementField();
+
+    /**
+     * @param string|array $fields
+     *
+     * @return \ManaPHP\Db\Model\CriteriaInterface
+     */
+    public static function createCriteria($fields = null);
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * <code>
+     *  $cities=City::find(['country_id'=>2]);
+     *  $cities=City::find(['conditions'=>['country_id'=>2],'order'=>'city_id desc']);
+     *  $cities=City::find([['country_id'=>2],'order'=>'city_id desc']);
+     *  $cities=City::find(['conditions'=>'country_id =:country_id','bind'=>['country_id'=>2]]);
+     *
+     * </code>
+     * @param array        $filters
+     * @param array        $options
+     * @param string|array $fields
+     *
+     * @return  static[]
+     */
+    public static function find($filters = [], $options = null, $fields = null);
+
+    /**
+     * alias of find
+     *
+     * @param array        $filters
+     * @param array        $options
+     * @param string|array $fields
+     *
+     * @return  static[]
+     */
+    public static function findAll($filters = [], $options = null, $fields = null);
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * <code>
+     *  $city=City::findFirst(10);
+     *  $city=City::findFirst(['city_id'=>10]);
+     *  $city=City::findFirst(['conditions'=>['city_id'=>10]]);
+     *  $city=City::findFirst(['conditions'=>'city_id =:city_id','bind'=>['city_id'=>10]]);
+     * </code>
+     *
+     * @param int|string|array $filters
+     * @param string|array     $fields
+     *
+     * @return static|false
+     */
+    public static function findFirst($filters = [], $fields = null);
+
+    /**
+     * @param int|string   $id
+     * @param string|array $fields
+     *
+     * @return static|false
+     */
+    public static function findById($id, $fields = null);
+
+    /**
+     * @param string|array $filters
+     *
+     * @return bool
+     */
+    public static function exists($filters = null);
+
+    /**
+     * @param int|string $id
+     *
+     * @return bool
+     */
+    public static function existsById($id);
+
+    /**
+     * Allows to count how many records match the specified conditions
+     *
+     * <code>
+     * City::count(['country_id'=>2]);
+     * </code>
+     *
+     * @param array  $filters
+     * @param string $field
+     *
+     * @return int
+     */
+    public static function count($filters = null, $field = null);
+
+    /**
+     * Allows to calculate a summary on a column that match the specified conditions
+     *
+     * @param string $field
+     * @param array  $filters
+     *
+     * @return int|float
+     */
+    public static function sum($field, $filters = null);
+
+    /**
+     * Allows to get the max value of a column that match the specified conditions
+     *
+     * @param string $field
+     * @param array  $filters
+     *
+     * @return int|float
+     */
+    public static function max($field, $filters = null);
+
+    /**
+     * Allows to get the min value of a column that match the specified conditions
+     *
+     * @param string $field
+     * @param array  $filters
+     *
+     * @return int|float
+     */
+    public static function min($field, $filters = null);
+
+    /**
+     * Allows to calculate the average value on a column matching the specified conditions
+     *
+     * @param string $field
+     * @param array  $filters
+     *
+     * @return double
+     */
+    public static function avg($field, $filters = null);
+
+    /**
+     * Assigns values to a model from an array
+     * <code>
+     *  $city->assign(['city_id'=>1,'city_name'=>'beijing']);
+     *  $city->assign(['city_id'=>1,'city_name'=>'beijing'],['city_name']);
+     * </code>
+     *
+     * @param array $data
+     * @param array $whiteList
+     *
+     * @return static
+     */
+    public function assign($data, $whiteList = null);
+
+    /**
+     * Inserts or updates a model instance. Returning true on success or false otherwise.
+     *
+     * @return void
+     */
+    public function save();
+
+    /**
+     * Inserts a model instance. If the instance already exists in the persistence it will throw an exception
+     * Returning true on success or false otherwise.
+     *
+     * @return void
+     */
+    public function create();
+
+    /**
+     * Updates a model instance. If the instance does n't exist in the persistence it will throw an exception
+     * Returning true on success or false otherwise.
+     *
+     * @return void
+     */
+    public function update();
+
+    /**
+     * @param int|string $id
+     * @param array      $data
+     * @param array      $whiteList
+     *
+     * @return int
+     */
+    public static function updateById($id, $data, $whiteList = null);
+
+    /**
+     * @param array $fieldValues
+     * @param array $filters
+     *
+     * @return int
+     */
+    public static function updateAll($fieldValues, $filters);
+
+    /**
+     * Deletes a model instance. Returning true on success or false otherwise.
+     *
+     * @return int
+     */
+    public function delete();
+
+    /**
+     * @param int|string $id
+     *
+     * @return void
+     */
+    public static function deleteById($id);
+
+    /**
+     * @param array $filters
+     *
+     * @return int
+     */
+    public static function deleteAll($filters);
+
+    /**
+     * Returns the instance as an array representation
+     *
+     *<code>
+     * print_r($robot->toArray());
+     *</code>
+     *
+     * @return array
+     */
+    public function toArray();
+
+    /**
+     * Returns the internal snapshot data
+     *
+     * @return array
+     */
+    public function getSnapshotData();
+
+    /**
+     * Returns a list of changed values
+     *
+     * @return array
+     */
+    public function getChangedFields();
+
+    /**
+     * Check if a specific attribute has changed
+     * This only works if the model is keeping data snapshots
+     *
+     * @param string|array $fields
+     *
+     * @return bool
+     */
+    public function hasChanged($fields);
+}
