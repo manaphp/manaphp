347508929608e07d5c323442927be0946039b79f
ManaPHP Mark <manaphp@qq.com>
2017-10-18 21:02:53 +0800

    Modified   public/index.php

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ ini_set('html_errors', 'on');
 
 define('ROOT_PATH', dirname(__DIR__));
 
-class_exists('ManaPHP\Loader') || require ROOT_PATH . '/ManaPHP/Loader.php';
-$loader = new \ManaPHP\Loader(ROOT_PATH . '/ManaPHP');
+require ROOT_PATH . '/ManaPHP/Loader.php';
+$loader = new \ManaPHP\Loader();
 
 require ROOT_PATH . '/Application/Application.php';
 $application = new \Application\Application($loader);
