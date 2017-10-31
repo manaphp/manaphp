e50e7b0cfb543a64f681bcb4651a319a9bbb3e0e
ManaPHP Mark <manaphp@qq.com>
2017-10-31 22:18:06 +0800

    Modified   Application/SwooleHttpServer.php

--- Application/SwooleHttpServer.php
+++ Application/SwooleHttpServer.php
@@ -8,6 +8,11 @@ class SwooleHttpServer extends \ManaPHP\Swoole\HttpServer
      */
     protected function _prepareSwoole($swoole)
     {
+
+        echo '   current_time: ', date('Y-m-d H:i:s'), PHP_EOL;
+        echo 'swoole lisen on: ', $this->_listen, PHP_EOL;
+
         $swoole->set(['worker_num' => 1, 'enable_static_handler' => true, 'document_root' => dirname(get_included_files()[0])]);
+
     }
 }
