dffd4362e2ec4d1e8cabf4e8dd000444037e7c75
ManaPHP Mark <manaphp@qq.com>
2017-11-04 10:58:47 +0800

    Modified   manacli.php

--- manacli.php
+++ manacli.php
@@ -4,28 +4,8 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/ManaPHP/Loader.php';
 $loader = new \ManaPHP\Loader();
+require __DIR__ . '/Application/Cli.php';
 
-foreach ([__DIR__, getcwd()] as $dir) {
-    if (is_dir($dir . '/Application')) {
-        $appDir = $dir . '/Application';
-        break;
-    } elseif (is_file($dir . '/Cli.php')) {
-        $appDir = $dir;
-        break;
-    }
-}
-
-if (!isset($appDir)) {
-    $appDir = getcwd();
-}
-
-$loader->registerNamespaces(['Application' => $appDir]);
-
-if (is_file($appDir . '/Cli.php')) {
-    require $appDir . '/Cli.php';
-    $cli = new \Application\Cli($loader);
-} else {
-    $cli = new \ManaPHP\Cli\Application($loader);
-}
+$cli = new \Application\Cli($loader);
 
 $cli->main();
