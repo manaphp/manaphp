72742925f521bab172bdfd2c40d77a5db8d23007
ManaPHP Mark <manaphp@qq.com>
2017-11-04 14:06:22 +0800

    Modified   Application/Cli.php

--- Application/Cli.php
+++ Application/Cli.php
@@ -5,5 +5,11 @@ use ManaPHP\Cli\Application;
 
 class Cli extends Application
 {
+    public function main()
+    {
+        $this->configure->load('@app/config.php', 'dev');
+        $this->configure->bootstraps = [];
 
+        parent::main();
+    }
 }
