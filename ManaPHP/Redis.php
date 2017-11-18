72192dcc271f8427bb122f9e20382c33d4c08684
ManaPHP Mark <manaphp@qq.com>
2017-11-18 09:20:49 +0800

    Modified   ManaPHP/Redis.php

--- ManaPHP/Redis.php
+++ ManaPHP/Redis.php
@@ -33,68 +33,45 @@ class Redis extends \Redis
     /**
      * @var int
      */
-    protected $_db;
+    protected $_db = 0;
 
     /**
      * Redis constructor.
      *
-     * @param array|string $options
+     * @param string $uri
      *
      * @throws \ManaPHP\Redis\Exception
      */
-    public function __construct($options)
+    public function __construct($uri = 'redis://127.0.0.1/1?timeout=3&retry_interval=0&auth=')
     {
-        if (is_string($options)) {
-            $url = $options;
+        $parts = parse_url($uri);
 
-            $parts = parse_url($options);
-
-            $options = [];
-
-            if ($parts['scheme'] !== 'redis') {
-                throw new RedisException('`:url` is invalid, `:scheme` scheme is not recognized', ['url' => $url, 'scheme' => $parts['scheme']]);
-            }
-
-            if (isset($parts['host'])) {
-                $options['host'] = $parts['host'];
-            }
-
-            if (isset($parts['port'])) {
-                $options['port'] = $parts['port'];
-            }
-
-            if (isset($parts['path']) && $parts['path'] !== '/') {
-                $path = trim($parts['path'], '/');
-                if (!is_numeric($path)) {
-                    throw new RedisException('`:url` url is invalid, `:db` db is not integer', ['url' => $url, 'db' => $path]);
-                }
-                $options['db'] = $path;
-            }
-
-            if (isset($parts['query'])) {
-                parse_str($parts['query'], $parts2);
-                if (isset($parts2['timeout'])) {
-                    $options['timeout'] = $parts2['timeout'];
-                }
-
-                if (isset($parts2['retry_interval'])) {
-                    $options['retry_interval'] = $parts2['retry_interval'];
-                }
-
-                if (isset($parts2['auth'])) {
-                    $options['auth'] = $parts2['auth'];
-                }
-            }
-        } elseif (is_object($options)) {
-            $options = (array)$options;
+        if ($parts['scheme'] !== 'redis') {
+            throw new RedisException('`:url` is invalid, `:scheme` scheme is not recognized', ['url' => $uri, 'scheme' => $parts['scheme']]);
         }
 
-        $this->_host = isset($options['host']) ? $options['host'] : 'localhost';
-        $this->_port = isset($options['port']) ? (int)$options['port'] : 6379;
-        $this->_timeout = isset($options['timeout']) ? (float)$options['timeout'] : 0.0;
-        $this->_retry_interval = isset($options['retry_interval']) ? (int)$options['retry_interval'] : 0;
-        $this->_auth = isset($options['auth']) ? $options['auth'] : '';
-        $this->_db = isset($options['db']) ? (int)$options['db'] : 0;
+        $this->_host = isset($parts['host']) ? $parts['host'] : '127.0.0.1';
+        $this->_port = isset($parts['port']) ? (int)$parts['port'] : 6379;
+
+        if (isset($parts['path'])) {
+            $path = trim($parts['path'], '/');
+            if ($path !== '') {
+                if (!is_numeric($path)) {
+                    throw new RedisException('`:url` url is invalid, `:db` db is not integer', ['url' => $uri, 'db' => $path]);
+                }
+            }
+            $this->_db = (int)$path;
+        }
+
+        if (isset($parts['query'])) {
+            parse_str($parts['query'], $parts2);
+        } else {
+            $parts2 = [];
+        }
+
+        $this->_timeout = isset($parts2['timeout']) ? (float)$parts2['timeout'] : 0.0;
+        $this->_retry_interval = isset($parts2['retry_interval']) ? (int)$parts2['retry_interval'] : 0;
+        $this->_auth = isset($parts2['auth']) ? $parts2['auth'] : '';
 
         parent::__construct();
 
@@ -108,16 +85,12 @@ class Redis extends \Redis
     {
         $this->connect($this->_host, $this->_port, $this->_timeout, null, $this->_retry_interval);
 
-        if ($this->_auth !== '') {
-            if (!$this->auth($this->_auth)) {
-                throw new RedisException('`:auth` auth is wrong.', ['auth' => $this->_auth]);
-            }
+        if ($this->_auth !== '' && !$this->auth($this->_auth)) {
+            throw new RedisException('`:auth` auth is wrong.', ['auth' => $this->_auth]);
         }
 
-        if ($this->_db !== 0) {
-            if (!$this->select($this->_db)) {
-                throw new RedisException('select `:db` db failed', ['db' => $this->_db]);
-            }
+        if ($this->_db !== 0 && !$this->select($this->_db)) {
+            throw new RedisException('select `:db` db failed', ['db' => $this->_db]);
         }
     }
 
