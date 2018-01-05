97478f5def56d15e094471811d56ce37f6845c95
ManaPHP Mark <manaphp@qq.com>
2018-01-05 21:17:13 +0800

    Modified   ManaPHP/Cache.php

--- ManaPHP/Cache.php
+++ ManaPHP/Cache.php
@@ -56,6 +56,7 @@ class Cache extends Component implements CacheInterface, ScopedCloneableInterfac
      * @param string $key
      *
      * @return mixed|false
+     * @throws \ManaPHP\Cache\Exception
      */
     public function get($key)
     {
@@ -146,6 +147,7 @@ class Cache extends Component implements CacheInterface, ScopedCloneableInterfac
      * @param callable $callback
      *
      * @return mixed
+     * @throws \ManaPHP\Cache\Exception
      */
     public function remember($key, $ttl, $callback)
     {
