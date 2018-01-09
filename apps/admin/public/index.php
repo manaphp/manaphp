b45f086ef86520a45b7c2453afa821e0e530f3c6
ManaPHP Mark <manaphp@qq.com>
2018-01-09 21:37:40 +0800

    Modified   apps/admin/public/index.php Modified   apps/cli/manacli Modified   apps/home/public/index.php

--- apps/admin/public/index.php
+++ apps/admin/public/index.php
@@ -1,16 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('html_errors', 'on');
-
-define('ROOT_PATH', __DIR__);
-
-if (PHP_EOL === "\n") {
-    require __DIR__ . '/../vendor/autoload.php';
-} else {
-    require __DIR__ . '/../../../ManaPHP/Loader.php';
-}
+//require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../../../ManaPHP/Loader.php';
 
 $loader = new \ManaPHP\Loader();
-require __DIR__.'/../app/Application.php';
+require dirname(__DIR__) . '/app/Application.php';
 $app = new \App\Admin\Application($loader);
 $app->main();
