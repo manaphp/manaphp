03d2f05aac56a6fe95a21e82169e8c6c3be78c4f
ManaPHP Mark <manaphp@qq.com>
2018-01-14 17:47:25 +0800

    Modified   apps/api/app/Models/Address.php

--- apps/api/app/Models/Address.php
+++ apps/api/app/Models/Address.php
@@ -1,6 +1,12 @@
 <?php
 namespace App\Api\Models;
 
+/**
+ * Class Address
+ * @package App\Api\Models
+ *
+ * @property \App\Api\Models\Customer[] $customers
+ */
 class Address extends ModelBase
 {
 
