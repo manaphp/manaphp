d362790035d500f84c554c8af8d2d5b63e36fde6
ManaPHP Mark <manaphp@qq.com>
2018-01-25 22:22:36 +0800

    Modified   apps/cli/app/Controllers/DbToJsonController.php

--- apps/cli/app/Controllers/DbToJsonController.php
+++ apps/cli/app/Controllers/DbToJsonController.php
@@ -10,12 +10,7 @@ class DbToJsonController extends Controller
     {
         foreach ($this->db->getTables() as $table) {
             $f = fopen("d:/manaphp_unit_test/$table.json", 'wb+');
-            $pk = $this->db->getMetadata($table)[Db::METADATA_PRIMARY_KEY];
             foreach ($this->db->fetchAll("SELECT * FROM [$table]") as $row) {
-                if (count($pk) === 1) {
-                    $row = ['_id' => $row[$pk[0]]] + $row;
-                }
-
                 fwrite($f, json_encode($row) . PHP_EOL);
             }
             fclose($f);
