df8f5bf11e18860716d328175ab44bfcd40f4b3f
ManaPHP Mark <manaphp@qq.com>
2018-02-11 10:40:05 +0800

    Modified   ManaPHP/Cli/Controller.php

--- ManaPHP/Cli/Controller.php
+++ ManaPHP/Cli/Controller.php
@@ -61,7 +61,7 @@ abstract class Controller extends Component implements ControllerInterface
                 list($tag, $description) = $parts;
                 $description = trim($description);
                 if ($tag === '@CliCommand') {
-                    $command = str_pad($controller . ':' . basename($method, 'Command'), 13) . ' ' . $description;
+                    $command = str_pad(basename($method, 'Command'), 10) . ' ' . $description;
                 } elseif ($tag === '@CliParam') {
                     $parts = explode(' ', $description, 2);
                     $params[trim($parts[0])] = isset($parts[1]) ? trim($parts[1]) : '';
@@ -88,7 +88,7 @@ abstract class Controller extends Component implements ControllerInterface
 
                 $maxLength = max(max(array_map('strlen', array_keys($params))), 1);
                 foreach ($params as $name => $value) {
-                    $this->console->writeLn('    ' . str_pad($name, $maxLength + 1, ' ') . ' ' . $value);
+                    $this->console->writeLn(' ' . str_pad($name, $maxLength + 1, ' ') . ' ' . $value);
                 }
             }
         }
