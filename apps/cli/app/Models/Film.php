1eec69fbec32bc78280e1f3c7bc54ab9ebd75c08
ManaPHP Mark <manaphp@qq.com>
2018-03-23 21:43:59 +0800

    Added      apps/cli/app/Models/Film.php

---
+++ apps/cli/app/Models/Film.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Cli\Models;
+
+use ManaPHP\Db\Model;
+
+class Film extends Model
+{
+    public $film_id;
+    public $title;
+    public $description;
+    public $release_year;
+    public $language_id;
+    public $original_language_id;
+    public $rental_duration;
+    public $rental_rate;
+    public $length;
+    public $replacement_cost;
+    public $rating;
+    public $special_features;
+    public $last_update;
+}
