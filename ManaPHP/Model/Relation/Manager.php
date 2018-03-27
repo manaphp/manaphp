10dd679dfe4bcd9bf170c1d963b53e28b0c86fd0
ManaPHP Mark <manaphp@qq.com>
2018-03-27 21:30:27 +0800

    Modified   ManaPHP/Model/Relation/Manager.php

--- ManaPHP/Model/Relation/Manager.php
+++ ManaPHP/Model/Relation/Manager.php
@@ -23,6 +23,27 @@ class Manager extends Component
     }
 
     /**
+     * @param string $str
+     *
+     * @return string|false
+     */
+    protected function _pluralToSingular($str)
+    {
+        if ($str[strlen($str) - 1] !== 's') {
+            return false;
+        }
+
+        //https://github.com/UlvHare/PHPixie-demo/blob/d000d8f11e6ab7c522feeb4457da5a802ca3e0bc/vendor/phpixie/orm/src/PHPixie/ORM/Configs/Inflector.php
+        if (preg_match('#^(.*?us)|(.*?[sxz])es|(.*?[^aeioudgkprt]h)es$#', $str, $match)) {
+            return $match[1];
+        } elseif (preg_match('#^(.*?[^aeiou])ies#', $str, $match)) {
+            return $match[1] . 'y';
+        } else {
+            return substr($str, 0, -1);
+        }
+    }
+
+    /**
      * @param \ManaPHP\Model $model
      * @param string         $name
      *
@@ -32,33 +53,44 @@ class Manager extends Component
     {
         $modelName = get_class($model);
 
-        if (in_array($name . '_id', $model->getFields(), true)) {
+        if ($singular = $this->_pluralToSingular($name)) {
             if (($pos = strrpos($modelName, '\\')) !== false) {
-                $className = substr($modelName, 0, $pos + 1) . ucfirst($name);
+                $className = substr($modelName, 0, $pos + 1) . ucfirst($singular);
             } else {
-                $className = ucfirst($name);
-            }
-
-            if (class_exists($className)) {
-                return [$className, Relation::TYPE_HAS_ONE];
-            }
-        } elseif (preg_match('#^(.*?)(ies|es|s)$#', $name, $match)) {
-            if ($match[2] === 'ies') {
-                $plainClassName = $match[1] . 'y';
-            } else {
-                $plainClassName = $match[1];
-            }
-
-            if (($pos = strrpos($modelName, '\\')) !== false) {
-                $className = substr($modelName, 0, $pos + 1) . ucfirst($plainClassName);
-            } else {
-                $className = ucfirst($plainClassName);
+                $className = ucfirst($singular);
             }
 
             return [$className, Relation::TYPE_HAS_MANY];
+        } else {
+            if (in_array($name . '_id', $model->getFields(), true)) {
+                if (($pos = strrpos($modelName, '\\')) !== false) {
+                    $className = substr($modelName, 0, $pos + 1) . ucfirst($name);
+                } else {
+                    $className = ucfirst($name);
+                }
+
+                if (class_exists($className)) {
+                    return [$className, Relation::TYPE_HAS_ONE];
+                }
+            }
         }
 
         return false;
+    }
+
+    /**
+     * @param string $str
+     *
+     * @return bool
+     */
+    protected function _isPlural($str)
+    {
+        if ($str[strlen($str) - 1] !== 's') {
+            return false;
+        }
+
+        //https://github.com/UlvHare/PHPixie-demo/blob/d000d8f11e6ab7c522feeb4457da5a802ca3e0bc/vendor/phpixie/orm/src/PHPixie/ORM/Configs/Inflector.php
+        return preg_match('#us|[sxz]es|[^aeioudgkprt]hes|[^aeiou]ies$#', $str) === 1;
     }
 
     /**
@@ -86,6 +118,9 @@ class Manager extends Component
             if ($relation instanceof Relation) {
                 return $relation;
             } else {
+                if (!isset($relation[1])) {
+                    $relation[1] = $this->_isPlural($name) ? Relation::TYPE_HAS_MANY : Relation::TYPE_HAS_ONE;
+                }
                 return $this->_relations[$modelName][$name] = new Relation($model, $relation);
             }
         }
