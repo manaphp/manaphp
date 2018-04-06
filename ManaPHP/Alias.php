d0f3bf7595557e51a9e6ef27e177e31e3a1b26a9
ManaPHP Mark <manaphp@qq.com>
2018-04-06 10:09:44 +0800

    Deleted    ManaPHP/Alias/Exception.php Modified   ManaPHP/Alias.php

--- ManaPHP/Alias.php
+++ ManaPHP/Alias.php
@@ -2,7 +2,7 @@
 
 namespace ManaPHP;
 
-use ManaPHP\Alias\Exception as AliasException;
+use ManaPHP\Exception\InvalidArgumentException;
 
 /**
  * Class ManaPHP\Alias
@@ -19,7 +19,6 @@ class Alias extends Component implements AliasInterface
     /**
      * Alias constructor.
      *
-     * @throws \ManaPHP\Alias\Exception
      */
     public function __construct()
     {
@@ -31,12 +30,11 @@ class Alias extends Component implements AliasInterface
      * @param string $path
      *
      * @return string
-     * @throws \ManaPHP\Alias\Exception
      */
     public function set($name, $path)
     {
         if ($name[0] !== '@') {
-            throw new AliasException(['`:name` must start with `@`'/**m02b52e71dba71561a*/, 'name' => $name]);
+            throw new InvalidArgumentException(['`:name` must start with `@`'/**m02b52e71dba71561a*/, 'name' => $name]);
         }
 
         if ($path === '') {
@@ -54,7 +52,6 @@ class Alias extends Component implements AliasInterface
      * @param string $name
      *
      * @return bool|string|array
-     * @throws \ManaPHP\Alias\Exception
      */
     public function get($name = null)
     {
@@ -63,7 +60,7 @@ class Alias extends Component implements AliasInterface
         }
 
         if ($name[0] !== '@') {
-            throw new AliasException(['`:name` must start with `@`'/**m0f809631289d02f8e*/, 'name' => $name]);
+            throw new InvalidArgumentException(['`:name` must start with `@`'/**m0f809631289d02f8e*/, 'name' => $name]);
         }
 
         return isset($this->_aliases[$name]) ? $this->_aliases[$name] : false;
@@ -73,12 +70,11 @@ class Alias extends Component implements AliasInterface
      * @param string $name
      *
      * @return bool
-     * @throws \ManaPHP\Alias\Exception
      */
     public function has($name)
     {
         if ($name[0] !== '@') {
-            throw new AliasException(['`:name` must start with `@`'/**m0f7f21386c79f1518*/, 'name' => $name]);
+            throw new InvalidArgumentException(['`:name` must start with `@`'/**m0f7f21386c79f1518*/, 'name' => $name]);
         }
 
         return isset($this->_aliases[$name]);
@@ -88,7 +84,6 @@ class Alias extends Component implements AliasInterface
      * @param string $path
      *
      * @return string
-     * @throws \ManaPHP\Alias\Exception
      */
     public function resolve($path)
     {
@@ -105,7 +100,7 @@ class Alias extends Component implements AliasInterface
 
         $alias = $parts[0];
         if (!isset($this->_aliases[$alias])) {
-            throw new AliasException(['`:alias` is not exists for `:path`'/**m0aac421937afe5850*/, 'alias' => $alias, 'path' => $path]);
+            throw new InvalidArgumentException(['`:alias` is not exists for `:path`'/**m0aac421937afe5850*/, 'alias' => $alias, 'path' => $path]);
         }
 
         return str_replace($alias, $this->_aliases[$alias], $path);
@@ -115,7 +110,6 @@ class Alias extends Component implements AliasInterface
      * @param string $ns
      *
      * @return string
-     * @throws \ManaPHP\Alias\Exception
      */
     public function resolveNS($ns)
     {
@@ -127,7 +121,7 @@ class Alias extends Component implements AliasInterface
 
         $alias = $parts[0];
         if (!isset($this->_aliases[$alias])) {
-            throw new AliasException(['`:alias` is not exists for `:namespace`'/**m0aac421937afe5850*/, 'alias' => $alias, 'namespace' => $ns]);
+            throw new InvalidArgumentException(['`:alias` is not exists for `:namespace`'/**m0aac421937afe5850*/, 'alias' => $alias, 'namespace' => $ns]);
         }
 
         return $this->_aliases[$alias] . (isset($parts[1]) ? '\\' . $parts[1] : '');
