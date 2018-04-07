fdced00c552156203c2116a4b3e2254da4f1821d
ManaPHP Mark <manaphp@qq.com>
2018-04-07 22:20:48 +0800

    Modified   ManaPHP/Alias.php

--- ManaPHP/Alias.php
+++ ManaPHP/Alias.php
@@ -2,7 +2,7 @@
 
 namespace ManaPHP;
 
-use ManaPHP\Exception\InvalidArgumentException;
+use ManaPHP\Exception\InvalidFormatException;
 
 /**
  * Class ManaPHP\Alias
@@ -34,7 +34,7 @@ class Alias extends Component implements AliasInterface
     public function set($name, $path)
     {
         if ($name[0] !== '@') {
-            throw new InvalidArgumentException(['`:name` must start with `@`'/**m02b52e71dba71561a*/, 'name' => $name]);
+            throw new InvalidFormatException(['`:name` must start with `@`'/**m02b52e71dba71561a*/, 'name' => $name]);
         }
 
         if ($path === '') {
@@ -60,7 +60,7 @@ class Alias extends Component implements AliasInterface
         }
 
         if ($name[0] !== '@') {
-            throw new InvalidArgumentException(['`:name` must start with `@`'/**m0f809631289d02f8e*/, 'name' => $name]);
+            throw new InvalidFormatException(['`:name` must start with `@`'/**m0f809631289d02f8e*/, 'name' => $name]);
         }
 
         return isset($this->_aliases[$name]) ? $this->_aliases[$name] : false;
@@ -74,7 +74,7 @@ class Alias extends Component implements AliasInterface
     public function has($name)
     {
         if ($name[0] !== '@') {
-            throw new InvalidArgumentException(['`:name` must start with `@`'/**m0f7f21386c79f1518*/, 'name' => $name]);
+            throw new InvalidFormatException(['`:name` must start with `@`'/**m0f7f21386c79f1518*/, 'name' => $name]);
         }
 
         return isset($this->_aliases[$name]);
@@ -100,7 +100,7 @@ class Alias extends Component implements AliasInterface
 
         $alias = $parts[0];
         if (!isset($this->_aliases[$alias])) {
-            throw new InvalidArgumentException(['`:alias` is not exists for `:path`'/**m0aac421937afe5850*/, 'alias' => $alias, 'path' => $path]);
+            throw new InvalidFormatException(['`:alias` is not exists for `:path`'/**m0aac421937afe5850*/, 'alias' => $alias, 'path' => $path]);
         }
 
         return str_replace($alias, $this->_aliases[$alias], $path);
@@ -121,7 +121,7 @@ class Alias extends Component implements AliasInterface
 
         $alias = $parts[0];
         if (!isset($this->_aliases[$alias])) {
-            throw new InvalidArgumentException(['`:alias` is not exists for `:namespace`'/**m0aac421937afe5850*/, 'alias' => $alias, 'namespace' => $ns]);
+            throw new InvalidFormatException(['`:alias` is not exists for `:namespace`'/**m0aac421937afe5850*/, 'alias' => $alias, 'namespace' => $ns]);
         }
 
         return $this->_aliases[$alias] . (isset($parts[1]) ? '\\' . $parts[1] : '');
