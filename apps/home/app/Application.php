9ca2b8e8fdf6eccd8dc1c3e0a6f46cf680dc1486
ManaPHP Mark <manaphp@qq.com>
2018-04-07 22:13:57 +0800

    Modified   apps/home/app/Application.php

--- apps/home/app/Application.php
+++ apps/home/app/Application.php
@@ -11,16 +11,6 @@ class Application extends \ManaPHP\Mvc\Application
     public function main()
     {
         $this->configure->loadFile('@app/config.php');
-
-        $this->registerServices();
-
-        try {
-            $this->handle();
-        } catch (\Exception $e) {
-            $this->errorHandler->handle($e);
-        }
-
-        $this->response->setHeader('X-Response-Time', round(microtime(true)-$_SERVER['REQUEST_TIME_FLOAT'],3));
-        $this->response->send();
+        parent::main();
     }
 }
