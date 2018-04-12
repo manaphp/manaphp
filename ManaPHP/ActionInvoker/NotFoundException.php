6a8afcde51e628fad3faefb55ba4e7a19f0135cd
ManaPHP Mark <manaphp@qq.com>
2018-04-12 21:30:14 +0800

    Modified   ManaPHP/ActionInvoker/NotFoundException.php

--- ManaPHP/ActionInvoker/NotFoundException.php
+++ ManaPHP/ActionInvoker/NotFoundException.php
@@ -3,5 +3,13 @@ namespace ManaPHP\ActionInvoker;
 
 class NotFoundException extends \ManaPHP\Exception
 {
+    public function getStatusCode()
+    {
+        return 404;
+    }
 
+    public function getStatusText()
+    {
+        return 'Not Found';
+    }
 }
