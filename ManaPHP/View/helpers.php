f6f1d1b35036b46d3bb6e14c8366f88d081f1f85
ManaPHP Mark <manaphp@qq.com>
2018-05-20 10:12:20 +0800

    Modified   ManaPHP/View/helpers.php Modified   ManaPHP/helpers.php

--- ManaPHP/View/helpers.php
+++ ManaPHP/View/helpers.php
@@ -101,16 +101,3 @@ if (!function_exists('partial')) {
         Di::getDefault()->renderer->partial($path, $vars);
     }
 }
-
-if (!function_exists('e')) {
-    /**
-     * @param string $value
-     * @param bool   $doubleEncode
-     *
-     * @return string
-     */
-    function e($value, $doubleEncode = true)
-    {
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', $doubleEncode);
-    }
-}
--- ManaPHP/helpers.php
+++ ManaPHP/helpers.php
@@ -508,3 +508,16 @@ if (!function_exists('render')) {
         return di('renderer')->render($file, $vars);
     }
 }
+
+if (!function_exists('e')) {
+    /**
+     * @param string $value
+     * @param bool   $doubleEncode
+     *
+     * @return string
+     */
+    function e($value, $doubleEncode = true)
+    {
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', $doubleEncode);
+    }
+}
