3ffbedce1ad2d3b858d336f42f766988e7736c30
ManaPHP Mark <manaphp@qq.com>
2018-05-27 10:08:48 +0800

    Added      apps/admin/.env.sample Modified   apps/admin/config/app.php

--- apps/admin/config/app.php
+++ apps/admin/config/app.php
@@ -1,27 +1,27 @@
 <?php
 
 return [
-    'debug' => true,
+    'env' => env('APP_ENV', 'prod'),
+    'debug' => env('APP_DEBUG', false),
     'version' => '1.1.1',
     'timezone' => 'PRC',
-    'master_key' => '',
+    'master_key' => env('MASTER_KEY'),
     'services' => [
         'pay' => ['key' => '124', 'secret' => 'abc'],
     ],
     'params' => ['manaphp_brand_show' => 1],
     'aliases' => [
-        '@xxx' => '@root/xvdfd'
     ],
     'components' => [
-        'db' => ['mysql://root@192.168.1.5/manaphp?charset=utf8'],
-        'redis' => ['redis://localhost:6379/1?timeout=2&retry_interval=0&auth=&persistent=0'],
-        'mongodb' => ['mongodb://127.0.0.1/manaphp_unit_test'],
+        'db' => [env('DB_URL')],
+        'redis' => [env('REDIS_URL')],
+        'mongodb' => [env('MONGODB_URL')],
         'logger' => [
-            'level' => 'debug',
+            'level' => env('LOGGER_LEVEL', 'info'),
             'appenders' => ['ManaPHP\Logger\Appender\File'],
         ],
         'translation' => ['language' => 'zh-CN,en'],
         'authorization' => \App\Admin\Areas\Rbac\Components\Rbac::class
     ],
-    'bootstraps' => ['debugger']
+    'bootstraps' => explode(',', env('BOOTSTRAPS'))
 ];
