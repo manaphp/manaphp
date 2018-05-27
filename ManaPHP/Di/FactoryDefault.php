4bbae6431b23f4fe5fa17165048a0d3f42b65245
ManaPHP Mark <manaphp@qq.com>
2018-05-27 17:34:31 +0800

    Modified   ManaPHP/Di/FactoryDefault.php Modified   ManaPHP/Rest/Swoole.php

--- ManaPHP/Di/FactoryDefault.php
+++ ManaPHP/Di/FactoryDefault.php
@@ -76,7 +76,8 @@ class FactoryDefault extends Di
             'rabbitmq' => 'ManaPHP\Amqp',
             'relationsManager' => 'ManaPHP\Model\Relation\Manager',
             'authenticationToken' => 'ManaPHP\Authentication\Token\Adapter\Mwt',
-            'mailer' => 'ManaPHP\Mailer\Adapter\File'
+            'mailer' => 'ManaPHP\Mailer\Adapter\File',
+            'swooleHttpServer' => 'ManaPHP\Swoole\HttpServer'
         ];
     }
 }
--- ManaPHP/Rest/Swoole.php
+++ ManaPHP/Rest/Swoole.php
@@ -10,10 +10,10 @@ use ManaPHP\Swoole\Exception as SwooleException;
  *
  * @package application
  *
- * @property \ManaPHP\Http\ResponseInterface  $response
- * @property \ManaPHP\RouterInterface         $router
- * @property \ManaPHP\Mvc\DispatcherInterface $dispatcher
- * @property \ManaPHP\Swoole\HttpStats        $httpStats
+ * @property \ManaPHP\Http\ResponseInterface     $response
+ * @property \ManaPHP\RouterInterface            $router
+ * @property \ManaPHP\Mvc\DispatcherInterface    $dispatcher
+ * @property \ManaPHP\Swoole\HttpServerInterface $swooleHttpServer
  */
 class Swoole extends Application
 {
@@ -29,45 +29,6 @@ class Swoole extends Application
         $this->_di->keepInstanceState();
     }
 
-    /**
-     * @param \swoole_http_request $request
-     */
-    protected function _prepareGlobals($request)
-    {
-        static $server = null;
-
-        if ($server === null) {
-            $server = [];
-            $script_filename = get_included_files()[0];
-            $server['DOCUMENT_ROOT'] = dirname($script_filename);
-            $server['SCRIPT_FILENAME'] = $script_filename;
-            $server['PHP_SELF'] = $server['SCRIPT_NAME'] = '/' . basename($script_filename);
-            $server['QUERY_STRING'] = '';
-            $server['REQUEST_SCHEME'] = 'http';
-            $parts = explode('-', phpversion());
-            $server['SERVER_SOFTWARE'] = 'Swoole/' . SWOOLE_VERSION . ' ' . php_uname('s') . '/' . $parts[1] . ' PHP/' . $parts[0];
-        }
-        $_SERVER = array_change_key_case($request->server, CASE_UPPER);
-        unset($_SERVER['SERVER_SOFTWARE']);
-
-        $_SERVER += $server;
-
-        foreach ($request->header ?: [] as $k => $v) {
-            $_SERVER['HTTP_' . strtoupper(strtr($k, '-', '_'))] = $v;
-        }
-
-        $_SERVER['WORKER_ID'] = $this->swoole->worker_pid;
-
-        $_GET = $request->get ?: [];
-        $_POST = $request->post ?: [];
-
-        /** @noinspection AdditionOperationOnArraysInspection */
-        $_REQUEST = $_POST + $_GET;
-
-        $_COOKIE = $request->cookie ?: [];
-        $_FILES = $request->files ?: [];
-    }
-
     protected function _beforeRequest()
     {
 
@@ -79,21 +40,9 @@ class Swoole extends Application
     }
 
     /**
-     * @param \swoole_http_request  $request
-     * @param \swoole_http_response $response
-     *
-     * @throws \ManaPHP\Swoole\Exception
      */
-    public function onRequest($request, $response)
+    public function handle()
     {
-        if ($request->server['request_uri'] === '/favicon.ico') {
-            $response->status(404);
-            $response->end();
-            return;
-        }
-
-        $this->_prepareGlobals($request);
-
         $this->_beforeRequest();
 
         $this->identity->authenticate();
@@ -103,21 +52,10 @@ class Swoole extends Application
         }
 
         $router = $this->router;
-
         $this->dispatcher->dispatch($router->getControllerName(), $router->getActionName(), $router->getParams());
-        $this->response->setHeader('X-Response-Time', round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 3));
-        $response->header('X-WORKER-ID', $_SERVER['WORKER_ID']);
-        $headers = $this->response->getHeaders();
-        if (isset($headers['Status'])) {
-            $parts = explode(' ', $headers['Status']);
-            $response->status($parts[0]);
-            unset($headers['Status']);
-        }
-        foreach ($headers as $k => $v) {
-            $response->header($k, $v);
-        }
-
-        $response->end($this->response->getContent());
+        $this->swooleHttpServer
+            ->sendHeaders($this->response->getHeaders())
+            ->sendContent($this->response->getContent());
         $this->_afterRequest();
         $this->_di->restoreInstancesState();
     }
@@ -132,13 +70,8 @@ class Swoole extends Application
             $this->configure->loadFile($this->_configFile);
         }
 
-        $swoole = new \swoole_http_server('0.0.0.0', 9501);
-        $this->_di->setShared('swoole', $swoole);
-        $swoole->set(['worker_num' => 2]);
-        $swoole->on('request', [$this, 'onRequest']);
-
         $this->registerServices();
 
-        $swoole->start();
+        $this->swooleHttpServer->start([$this, 'handle']);
     }
 }
