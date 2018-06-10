6fe26e51acf95c39041fb5b83dc46812f9176a79
ManaPHP Mark <manaphp@qq.com>
2018-06-10 17:16:03 +0800

    Modified   ManaPHP/helpers.php

--- ManaPHP/helpers.php
+++ ManaPHP/helpers.php
@@ -787,3 +787,29 @@ if (!function_exists('mbstr_contains')) {
         return mb_strpos($haystack, $needle) !== false;
     }
 }
+
+if (!function_exists('str_in_set')) {
+    /**
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @return bool
+     */
+    function str_in_set($needle, $haystack)
+    {
+        return preg_match('#\b' . preg_quote($needle, '#') . '\b#', $haystack) === 1;
+    }
+}
+
+if (!function_exists('mbstr_in_set')) {
+    /**
+     * @param string $needle
+     * @param string $haystack
+     *
+     * @return bool
+     */
+    function mbstr_in_set($needle, $haystack)
+    {
+        return preg_match('#\b' . preg_quote($needle, '#') . '\b#u', $haystack) === 1;
+    }
+}
