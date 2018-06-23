9c2225eda3ddef75e57754c618e4326b7aef5ddd
ManaPHP Mark <manaphp@qq.com>
2018-06-23 22:04:46 +0800

    Modified   ManaPHP/Cli/Controllers/PasswordController.php

--- ManaPHP/Cli/Controllers/PasswordController.php
+++ ManaPHP/Cli/Controllers/PasswordController.php
@@ -6,25 +6,40 @@ use ManaPHP\Cli\Controller;
 /**
  * Class PasswordController
  * @package ManaPHP\Cli\Controllers
- * @property \ManaPHP\Authentication\PasswordInterface $password
  */
 class PasswordController extends Controller
 {
     /**
-     *  calculate password hash
-     *
-     * @param string $salt
+     * @param int    $length
      * @param string $password
+     * @param int    $base
+     * @param int    $cost
      */
-    public function hashCommand($salt = '', $password = '')
+    public function generateCommand($length = 32, $password = '', $base = 62, $cost = 0)
     {
-        if ($salt === '') {
-            $salt = $this->password->salt();
-        } elseif (is_numeric($salt) && $salt < 100) {
-            $salt = $this->password->salt($salt);
+        if ($password === '') {
+            $password = $this->random->getBase($length, $base);
         }
 
-        $this->console->writeLn('salt: ' . $salt);
-        $this->console->writeLn('hash: ' . $this->password->hash($password, $salt));
+        $this->console->writeLn('password: ' . $password);
+
+        if ($cost) {
+            $this->console->writeLn('hash: ' . password_hash($password, PASSWORD_BCRYPT, ['cost' => $cost]));
+        } else {
+            for ($i = 7; $i < 14; $i++) {
+                $start = microtime(true);
+                $hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => $i]);
+                $this->console->writeLn([':time: :hash', 'time' => sprintf('%.3f', microtime(true) - $start), 'hash' => $hash]);
+            }
+        }
+    }
+
+    public function costCommand()
+    {
+        for ($i = 7; $i < 14; $i++) {
+            $start = microtime(true);
+            password_hash('password', PASSWORD_BCRYPT, ['cost' => $i]);
+            $this->console->writeLn(sprintf('%2d: %.3f', $i, microtime(true) - $start));
+        }
     }
 }
