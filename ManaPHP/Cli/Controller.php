55cecf6f18c9c2a17183dd3abd61178b0d94ce97
ManaPHP Mark <manaphp@qq.com>
2018-07-29 21:09:44 +0800

    Modified   ManaPHP/Cli/Controller.php

--- ManaPHP/Cli/Controller.php
+++ ManaPHP/Cli/Controller.php
@@ -33,8 +33,10 @@ abstract class Controller extends Component implements ControllerInterface, LogC
     public function helpCommand()
     {
         $args = $this->cliHandler->getArgs();
-        if (isset($args[2]) && $args[2][0] !== '-') {
+        if (isset($args[2]) && $args[2][0] !== '-' && $args[2] !== 'help') {
             $actionName = $args[2];
+        } elseif (isset($args[3]) && $args[3][0] !== '-' && $args[2] === 'help') {
+            $actionName = $args[3];
         } else {
             $actionName = '';
         }
