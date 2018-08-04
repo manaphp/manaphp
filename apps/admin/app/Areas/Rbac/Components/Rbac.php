71ad5072d64ab8bf0c53fcbc09d6747df212327e
ManaPHP Mark <manaphp@qq.com>
2018-08-04 22:58:50 +0800

    Modified   apps/admin/app/Areas/Rbac/Components/Rbac.php

--- apps/admin/app/Areas/Rbac/Components/Rbac.php
+++ apps/admin/app/Areas/Rbac/Components/Rbac.php
@@ -7,7 +7,7 @@ use App\Admin\Areas\Rbac\Models\RolePermission;
 use App\Admin\Areas\Rbac\Models\AdminRole;
 use ManaPHP\AuthorizationInterface;
 use ManaPHP\Component;
-use App\Admin\Areas\Rbac\Components\Rbac\Exception as RbacException;
+use ManaPHP\Exception\InvalidValueException;
 
 /**
  *
@@ -30,24 +30,22 @@ class Rbac extends Component implements AuthorizationInterface
         $permission = Permission::first(['path' => $permissionName]);
 
         if (!$permission) {
-            throw new RbacException(['`:permission` permission is not exists'/**m06ab9af781c2de7f2*/, 'permission' => $permissionName]);
+            throw new InvalidValueException(['`:permission` permission is not exists'/**m06ab9af781c2de7f2*/, 'permission' => $permissionName]);
         }
 
         switch ($permission->type) {
             case Permission::TYPE_PENDING:
-                throw new RbacException(['`:permission` type is not assigned'/**m0ac1449c071933ff6*/, 'permission' => $permission->description]);
+                throw new InvalidValueException(['`:permission` type is not assigned'/**m0ac1449c071933ff6*/, 'permission' => $permission->description]);
             case Permission::TYPE_PUBLIC:
                 return true;
             case Permission::TYPE_INTERNAL:
                 return !empty($userId);
-            case Permission::TYPE_DISABLED:
-                return false;
             case Permission::TYPE_PRIVATE:
                 $rolesByPermissionId = RolePermission::values('role_id', ['permission_id' => $permission->permission_id]);
                 $rolesByUserId = AdminRole::values('role_id', ['admin_id' => $userId]);
                 return (bool)array_intersect($rolesByPermissionId, $rolesByUserId);
             default:
-                throw new RbacException(['`:permission` type is not recognized', 'permission' => $permissionName]);
+                throw new InvalidValueException(['`:permission` type is not recognized', 'permission' => $permissionName]);
         }
     }
 }
