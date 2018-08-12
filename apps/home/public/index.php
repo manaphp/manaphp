1f2b0ecedcd7f019ffe85905516a08a9f0ba0881
ManaPHP Mark <manaphp@qq.com>
2018-08-12 21:55:28 +0800

    Added      apps/home/.env.sample Added      apps/home/docker/fpm/conf.d/fpm.conf Added      apps/home/docker/fpm/conf.d/php.ini Modified   apps/home/docker/.env Modified   apps/home/docker/.env-dev Modified   apps/home/docker/.env-prod Modified   apps/home/docker/.env-stage Modified   apps/home/docker/.env-test Modified   apps/home/docker/docker-compose.yml Modified   apps/home/docker/nginx/conf/nginx.conf Modified   apps/home/docker/nginx/docker-entrypoint.sh Missing    apps/home/docker/phpfpm/conf/www.conf Missing    apps/home/docker/phpfpm/conf/www.conf.default Missing    apps/home/docker/phpfpm/docker-entrypoint.d/current_time.php Modified   apps/home/public/index.php

--- apps/home/public/index.php
+++ apps/home/public/index.php
@@ -1,6 +1,7 @@
 <?php
 //require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../../../ManaPHP/Loader.php';
+//require dirname(__DIR__).'/vendor/manaphp/framework/Loader.php';
 
 $loader = new \ManaPHP\Loader();
 require dirname(__DIR__) . '/app/Application.php';
