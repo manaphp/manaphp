5e032a46fe84436d6de9c5134110530a726cc109
ManaPHP Mark <manaphp@qq.com>
2018-08-12 21:33:02 +0800

    Modified   apps/api/config/app.php

--- apps/api/config/app.php
+++ apps/api/config/app.php
@@ -1,26 +1,21 @@
 <?php
 
 return [
-    'debug' => true,
+    'env' => env('APP_ENV', 'prod'),
+    'debug' => env('APP_DEBUG', false),
     'version' => '1.1.1',
     'timezone' => 'PRC',
-    'master_key' => '',
-    'services' => [
-        'pay' => ['key' => '124', 'secret' => 'abc'],
-    ],
-    'params' => ['manaphp_brand_show' => 1],
-    'aliases' => [
-        '@xxx' => '@root/xvdfd'
-    ],
+    'master_key' => env('MASTER_KEY'),
+    'services' => [],
+    'params' => [],
+    'aliases' => [],
     'components' => [
-        'db' => 'mysql://root@192.168.1.5/manaphp?charset=utf8',
-        'redis' => 'redis://localhost:6379/1?timeout=2&retry_interval=0&auth=&persistent=0',
-        'mongodb' => 'mongodb://127.0.0.1/manaphp_unit_test',
+        'db' => [env('DB_URL')],
+        'redis' => [env('REDIS_URL')],
+        'mongodb' => [env('MONGODB_URL')],
         'logger' => [
-            'level' => 'debug',
-            'appenders' => ['ManaPHP\Logger\Appender\File'],
-        ],
-        'translation' => ['language' => 'zh-CN,en']
+            'level' => env('LOGGER_LEVEL', 'info'),
+            'appenders' => ['file' => ['file' => '@data/logger/log.log']],
+        ]
     ],
-    'bootstraps' => ['debugger']
 ];
