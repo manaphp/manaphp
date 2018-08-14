6ffe847fbb4c5635e01caadf8eb2ba5be644334e
ManaPHP Mark <manaphp@qq.com>
2018-08-14 21:54:01 +0800

    Modified   apps/admin/manacli.php Modified   apps/api/manacli.php Modified   apps/cli/manacli.php Modified   apps/home/manacli.php

--- apps/home/manacli.php
+++ apps/home/manacli.php
@@ -1,10 +1,9 @@
 #!/usr/bin/env php
 <?php
+/** @noinspection PhpIncludeInspection */
 require __DIR__ . (is_dir(__DIR__ . '/vendor/') ? '/vendor/manaphp/framework' : '/../../ManaPHP') . '/Loader.php';
-
-if (is_file(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
-}
+/** @noinspection PhpIncludeInspection */
+is_file(__DIR__ . '/vendor/autoload.php') && require __DIR__ . '/vendor/autoload.php';
 
 $loader = new \ManaPHP\Loader();
 $cli = new \ManaPHP\Cli\Application($loader);
