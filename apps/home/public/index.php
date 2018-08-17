57326b29dc2ea915fcfb0d01517cdae1813af7a3
ManaPHP Mark <manaphp@qq.com>
2018-08-17 21:28:56 +0800

    Modified   apps/home/public/index.php

--- apps/home/public/index.php
+++ apps/home/public/index.php
@@ -1,9 +1,12 @@
 <?php
-//require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../../../ManaPHP/Loader.php';
-//require dirname(__DIR__).'/vendor/manaphp/framework/Loader.php';
+chdir(dirname(__DIR__));
+
+/** @noinspection PhpIncludeInspection */
+is_file('vendor/autoload.php') && require 'vendor/autoload.php';
+/** @noinspection PhpIncludeInspection */
+require (is_dir('vendor/manaphp/framework/') ? 'vendor/manaphp/framework' : '../../ManaPHP') . '/Loader.php';
 
 $loader = new \ManaPHP\Loader();
-require dirname(__DIR__) . '/app/Application.php';
+require 'app/Application.php';
 $app = new \App\Home\Application($loader);
 $app->main();
