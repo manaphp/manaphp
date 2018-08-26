65f180f69063059a5175ac88f263a50d005c753b
ManaPHP Mark <manaphp@qq.com>
2018-08-26 08:57:11 +0800

    Modified   apps/api/app/Router.php

--- apps/api/app/Router.php
+++ apps/api/app/Router.php
@@ -9,6 +9,7 @@ class Router extends \ManaPHP\Router
     public function __construct()
     {
         parent::__construct(false);
+        $this->addGet('/', [TimeController::class, 'current']);
         $this->addGet('/time/current', [TimeController::class, 'current']);
         $this->addGet('/time/timestamp', [TimeController::class, 'timestamp']);
         $this->addRest('/customers', CustomerController::class);
