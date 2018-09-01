5d3998bcc173775cc0a313865c08e507fd30ae02
ManaPHP Mark <manaphp@qq.com>
2018-09-01 21:03:32 +0800

    Modified   ManaPHP/View/Url.php

--- ManaPHP/View/Url.php
+++ ManaPHP/View/Url.php
@@ -8,31 +8,9 @@ use ManaPHP\Component;
  *
  * @package url
  *
- * @property \ManaPHP\Configuration\Configure $configure
- * @property \ManaPHP\Http\RequestInterface   $request
- * @property \ManaPHP\RouterInterface         $router
- * @property \ManaPHP\Mvc\DispatcherInterface $dispatcher
  */
 class Url extends Component implements UrlInterface
 {
-    /**
-     * @var string
-     */
-    protected $_prefix;
-
-    /**
-     * Url constructor.
-     *
-     * @param array $options
-     */
-    public function __construct($options = [])
-    {
-        $this->_prefix = $this->router->getPrefix();
-        if ($this->_prefix[0] === '/') {
-            $this->_prefix = rtrim($this->alias->resolve('@web') . $this->_prefix, '/');
-        }
-    }
-
     /**
      * @param string|array $args
      *
@@ -55,10 +33,11 @@ class Url extends Component implements UrlInterface
             }
         }
 
+        $prefix = $this->alias->resolve('@web');
         if ($uri === '' || $uri[0] !== '/') {
-            $strUrl = (strpos($this->_prefix, '://') ? parse_url($this->_prefix, PHP_URL_PATH) : $this->_prefix) . '/' . $uri;
+            $strUrl = (strpos($prefix, '://') ? parse_url($prefix, PHP_URL_PATH) : $prefix) . '/' . $uri;
         } else {
-            $strUrl = ($this->_prefix === '/' ? '' : rtrim($this->_prefix, '/')) . $uri;
+            $strUrl = ($prefix === '/' ? '' : rtrim($prefix, '/')) . $uri;
         }
 
         if (strpos($strUrl, ':') !== false) {
