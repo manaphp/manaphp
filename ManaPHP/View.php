effe86ac732e4348c19807f4e30252adaffa51bf
ManaPHP Mark <manaphp@qq.com>
2018-09-08 17:13:50 +0800

    Modified   ManaPHP/View.php

--- ManaPHP/View.php
+++ ManaPHP/View.php
@@ -196,7 +196,7 @@ class View extends Component implements ViewInterface
     public function getWidgetClassName($widget)
     {
         if (strpos($widget, '/') !== false) {
-            throw new MisuseException('it is not allowed to access other area widgets');
+            throw new MisuseException(['it is not allowed to access other area `:widget` widget', 'widget' => $widget]);
         }
 
         $controllerName = $this->dispatcher->getControllerName();
