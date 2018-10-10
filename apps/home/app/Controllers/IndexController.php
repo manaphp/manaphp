dc8f79204346b04b644fc1b68c5b1032257fc3bd
ManaPHP Mark <manaphp@qq.com>
2018-10-10 21:46:15 +0800

    Modified   apps/home/app/Controllers/IndexController.php

--- apps/home/app/Controllers/IndexController.php
+++ apps/home/app/Controllers/IndexController.php
@@ -7,7 +7,7 @@ class IndexController extends ControllerBase
 {
     public function indexAction()
     {
-        $this->response->redirect('about');
+        return $this->response->redirect('about');
     }
 
     /**
