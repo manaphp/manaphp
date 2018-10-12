bc0d727c66903631d7d57a4d95360fe728e9333a
ManaPHP Mark <manaphp@qq.com>
2018-10-12 23:28:45 +0800

    Modified   apps/cli/app/Controllers/TestController.php

--- apps/cli/app/Controllers/TestController.php
+++ apps/cli/app/Controllers/TestController.php
@@ -10,7 +10,6 @@ class TestController extends Controller
      */
     public function defaultCommand()
     {
-        var_dump($this->environment->get('PATH'));
         var_dump(get_included_files());
     }
 }
