2e767e0742e61e71801a1d7b377918c3dd74075d
ManaPHP Mark <manaphp@qq.com>
2018-10-16 22:04:16 +0800

    Modified   apps/home/app/Application.php

--- apps/home/app/Application.php
+++ apps/home/app/Application.php
@@ -4,5 +4,13 @@ namespace App;
 
 class Application extends \ManaPHP\Mvc\Application
 {
+    public function authenticate()
+    {
+     //   return parent::authenticate();
+    }
 
+    public function authorize()
+    {
+      //  parent::authorize();
+    }
 }
