cd3f12b9a73f7da249cbefdfa5829e33ba74661e
ManaPHP Mark <manaphp@qq.com>
2018-10-18 21:18:34 +0800

    Modified   ManaPHP/Mvc/Controller.php Modified   ManaPHP/Rest/Controller.php

--- ManaPHP/Mvc/Controller.php
+++ ManaPHP/Mvc/Controller.php
@@ -10,32 +10,32 @@ namespace ManaPHP\Mvc;
  * @method bool beforeExecuteRoute();
  * @method bool afterExecuteRoute();
  *
- * @property-read \ManaPHP\ViewInterface                    $view
- * @property-read \ManaPHP\View\FlashInterface              $flash
- * @property-read \ManaPHP\View\FlashInterface              $flashSession
- * @property-read \ManaPHP\Security\CaptchaInterface        $captcha
- * @property-read \ManaPHP\Authentication\PasswordInterface $password
- * @property-read \ManaPHP\Http\CookiesInterface            $cookies
- * @property-read \ManaPHP\CounterInterface                 $counter
- * @property-read \ManaPHP\Http\RequestInterface            $request
- * @property-read \ManaPHP\Http\ResponseInterface           $response
- * @property-read \ManaPHP\Mvc\DispatcherInterface          $dispatcher
- * @property-read \ManaPHP\Http\SessionInterface            $session
- * @property-read \ManaPHP\Security\CsrfTokenInterface      $csrfToken
- * @property-read \ManaPHP\Paginator                        $paginator
- * @property-read \ManaPHP\Cache\EngineInterface            $viewsCache
- * @property-read \ManaPHP\Message\QueueInterface           $messageQueue
- * @property-read \ManaPHP\Security\RateLimiterInterface    $rateLimiter
- * @property-read \ManaPHP\Meter\LinearInterface            $linearMeter
- * @property-read \ManaPHP\Meter\RoundInterface             $roundMeter
- * @property-read \ManaPHP\Security\SecintInterface         $secint
- * @property-read \ManaPHP\Http\FilterInterface             $filter
- * @property-read \ManaPHP\Db\Model\MetadataInterface       $modelsMetadata
- * @property-read \ManaPHP\UrlInterface                     $url
- * @property-read \ManaPHP\StopwatchInterface               $stopwatch
- * @property-read \ManaPHP\Security\HtmlPurifierInterface   $htmlPurifier
- * @property-read \ManaPHP\Net\ConnectivityInterface        $netConnectivity
- * @property-read \ManaPHP\RouterInterface                  $router
+ * @property-read \ManaPHP\ViewInterface                  $view
+ * @property-read \ManaPHP\View\FlashInterface            $flash
+ * @property-read \ManaPHP\View\FlashInterface            $flashSession
+ * @property-read \ManaPHP\Security\CaptchaInterface      $captcha
+ * @property-read \ManaPHP\PasswordInterface              $password
+ * @property-read \ManaPHP\Http\CookiesInterface          $cookies
+ * @property-read \ManaPHP\CounterInterface               $counter
+ * @property-read \ManaPHP\Http\RequestInterface          $request
+ * @property-read \ManaPHP\Http\ResponseInterface         $response
+ * @property-read \ManaPHP\Mvc\DispatcherInterface        $dispatcher
+ * @property-read \ManaPHP\Http\SessionInterface          $session
+ * @property-read \ManaPHP\Security\CsrfTokenInterface    $csrfToken
+ * @property-read \ManaPHP\Paginator                      $paginator
+ * @property-read \ManaPHP\Cache\EngineInterface          $viewsCache
+ * @property-read \ManaPHP\Message\QueueInterface         $messageQueue
+ * @property-read \ManaPHP\Security\RateLimiterInterface  $rateLimiter
+ * @property-read \ManaPHP\Meter\LinearInterface          $linearMeter
+ * @property-read \ManaPHP\Meter\RoundInterface           $roundMeter
+ * @property-read \ManaPHP\Security\SecintInterface       $secint
+ * @property-read \ManaPHP\Http\FilterInterface           $filter
+ * @property-read \ManaPHP\Db\Model\MetadataInterface     $modelsMetadata
+ * @property-read \ManaPHP\UrlInterface                   $url
+ * @property-read \ManaPHP\StopwatchInterface             $stopwatch
+ * @property-read \ManaPHP\Security\HtmlPurifierInterface $htmlPurifier
+ * @property-read \ManaPHP\Net\ConnectivityInterface      $netConnectivity
+ * @property-read \ManaPHP\RouterInterface                $router
  */
 abstract class Controller extends \ManaPHP\Controller
 {
--- ManaPHP/Rest/Controller.php
+++ ManaPHP/Rest/Controller.php
@@ -10,24 +10,24 @@ namespace ManaPHP\Rest;
  * @method bool beforeExecuteRoute();
  * @method bool afterExecuteRoute();
  *
- * @property-read \ManaPHP\Security\CaptchaInterface        $captcha
- * @property-read \ManaPHP\Authentication\PasswordInterface $password
- * @property-read \ManaPHP\CounterInterface                 $counter
- * @property-read \ManaPHP\Http\RequestInterface            $request
- * @property-read \ManaPHP\Http\ResponseInterface           $response
- * @property-read \ManaPHP\Mvc\DispatcherInterface          $dispatcher
- * @property-read \ManaPHP\Security\CsrfTokenInterface      $csrfToken
- * @property-read \ManaPHP\Paginator                        $paginator
- * @property-read \ManaPHP\Message\QueueInterface           $messageQueue
- * @property-read \ManaPHP\Security\RateLimiterInterface    $rateLimiter
- * @property-read \ManaPHP\Meter\LinearInterface            $linearMeter
- * @property-read \ManaPHP\Meter\RoundInterface             $roundMeter
- * @property-read \ManaPHP\Security\SecintInterface         $secint
- * @property-read \ManaPHP\Http\FilterInterface             $filter
- * @property-read \ManaPHP\Db\Model\MetadataInterface       $modelsMetadata
- * @property-read \ManaPHP\StopwatchInterface               $stopwatch
- * @property-read \ManaPHP\Security\HtmlPurifierInterface   $htmlPurifier
- * @property-read \ManaPHP\RouterInterface                  $router
+ * @property-read \ManaPHP\Security\CaptchaInterface      $captcha
+ * @property-read \ManaPHP\PasswordInterface              $password
+ * @property-read \ManaPHP\CounterInterface               $counter
+ * @property-read \ManaPHP\Http\RequestInterface          $request
+ * @property-read \ManaPHP\Http\ResponseInterface         $response
+ * @property-read \ManaPHP\Mvc\DispatcherInterface        $dispatcher
+ * @property-read \ManaPHP\Security\CsrfTokenInterface    $csrfToken
+ * @property-read \ManaPHP\Paginator                      $paginator
+ * @property-read \ManaPHP\Message\QueueInterface         $messageQueue
+ * @property-read \ManaPHP\Security\RateLimiterInterface  $rateLimiter
+ * @property-read \ManaPHP\Meter\LinearInterface          $linearMeter
+ * @property-read \ManaPHP\Meter\RoundInterface           $roundMeter
+ * @property-read \ManaPHP\Security\SecintInterface       $secint
+ * @property-read \ManaPHP\Http\FilterInterface           $filter
+ * @property-read \ManaPHP\Db\Model\MetadataInterface     $modelsMetadata
+ * @property-read \ManaPHP\StopwatchInterface             $stopwatch
+ * @property-read \ManaPHP\Security\HtmlPurifierInterface $htmlPurifier
+ * @property-read \ManaPHP\RouterInterface                $router
  */
 abstract class Controller extends \ManaPHP\Controller
 {
