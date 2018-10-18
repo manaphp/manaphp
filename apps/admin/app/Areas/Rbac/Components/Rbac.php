d006ee6973a4b9a88b2a38c8c83760b2bc9e5d69
ManaPHP Mark <manaphp@qq.com>
2018-10-18 23:00:55 +0800

    Modified   apps/admin/app/Areas/Rbac/Components/Rbac.php

--- apps/admin/app/Areas/Rbac/Components/Rbac.php
+++ apps/admin/app/Areas/Rbac/Components/Rbac.php
@@ -23,13 +23,42 @@ class Rbac extends Authorization
     public function isAllowed($permission = null, $role = null)
     {
         $role = $role ?: $this->identity->getRole();
-        $permissionModel = Permission::first(['path' => $permission]);
-
-        if (!$permissionModel) {
-            throw new InvalidValueException(['`:permission` permission is not exists'/**m06ab9af781c2de7f2*/, 'permission' => $permission]);
+        if ($role === 'admin') {
+            return true;
         }
 
-        $rolesByPermissionId = RolePermission::values('role_id', ['permission_id' => $permissionModel->permission_id]);
-        return Role::exists(['role_name' => $role, 'role_id' => $rolesByPermissionId]);
+        if ($permission && strpos($permission, '/') !== false) {
+            list($controllerClassName, $action) = $this->inferControllerAction($permission);
+            $controllerClassName = $this->alias->resolveNS($controllerClassName);
+            if (!isset($this->_acl[$controllerClassName])) {
+                /** @var \ManaPHP\Controller $controllerInstance */
+                $controllerInstance = new $controllerClassName;
+                $this->_acl[$controllerClassName] = $controllerInstance->getAcl();
+            }
+        } else {
+            $controllerInstance = $this->dispatcher->getController();
+            $controllerClassName = get_class($controllerInstance);
+            $action = $permission ?: $this->dispatcher->getActionName();
+
+            if (!isset($this->_acl[$controllerClassName])) {
+                $this->_acl[$controllerClassName] = $controllerInstance->getAcl();
+            }
+        }
+
+        $acl = $this->_acl[$controllerClassName];
+        if ($role === 'guest') {
+            return $this->isAllowRoleAction($acl, 'guest', $action);
+        } elseif ($this->isAllowRoleAction($acl, 'user', $action)) {
+            return true;
+        } else {
+            $permissionModel = Permission::first(['path' => $permission]);
+
+            if (!$permissionModel) {
+                throw new InvalidValueException(['`:permission` permission is not exists'/**m06ab9af781c2de7f2*/, 'permission' => $permission]);
+            }
+
+            $rolesByPermissionId = RolePermission::values('role_id', ['permission_id' => $permissionModel->permission_id]);
+            return Role::exists(['role_name' => $role, 'role_id' => $rolesByPermissionId]);
+        }
     }
 }
