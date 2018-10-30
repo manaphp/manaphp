ad9c771130069c32bfc7f69fa0f091394f600d7f
ManaPHP Mark <manaphp@qq.com>
2018-10-30 21:42:17 +0800

    Added      ManaPHP/Renderer/Listener.php

---
+++ ManaPHP/Renderer/Listener.php
@@ -0,0 +1,27 @@
+<?php
+namespace ManaPHP\Renderer;
+
+class Listener extends \ManaPHP\Event\Listener
+{
+    /**
+     * @param \ManaPHP\RendererInterface $renderer
+     * @param array                      $data
+     *
+     * @return void
+     */
+    public function onBeforeRender($renderer, $data)
+    {
+
+    }
+
+    /**
+     * @param \ManaPHP\RendererInterface $renderer
+     * @param array                      $data
+     *
+     * @return void
+     */
+    public function onAfterRender($renderer, $data)
+    {
+
+    }
+}
