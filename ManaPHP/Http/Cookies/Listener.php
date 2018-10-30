00dde6b5d6f7777f0e6ed89204fe6505508eac36
ManaPHP Mark <manaphp@qq.com>
2018-10-30 21:46:24 +0800

    Added      ManaPHP/Http/Cookies/Listener.php

---
+++ ManaPHP/Http/Cookies/Listener.php
@@ -0,0 +1,25 @@
+<?php
+namespace ManaPHP\Http\Cookies;
+
+class Listener extends \ManaPHP\Event\Listener
+{
+    /**
+     * @param \ManaPHP\Http\CookiesInterface $cookies
+     *
+     * @return void
+     */
+    public function onBeforeSend($cookies)
+    {
+
+    }
+
+    /**
+     * @param \ManaPHP\Http\CookiesInterface $cookies
+     *
+     * @return void
+     */
+    public function onAfterSend($cookies)
+    {
+
+    }
+}
