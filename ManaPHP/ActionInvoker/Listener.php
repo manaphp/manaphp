0d2025302571bcb8b7dcb6d9a82d9fe9716ca29b
ManaPHP Mark <manaphp@qq.com>
2018-10-30 21:49:25 +0800

    Added      ManaPHP/ActionInvoker/Listener.php

---
+++ ManaPHP/ActionInvoker/Listener.php
@@ -0,0 +1,25 @@
+<?php
+namespace ManaPHP\ActionInvoker;
+
+class Listener extends \ManaPHP\Event\Listener
+{
+    /**
+     * @param \ManaPHP\ActionInvokerInterface $actionInvoker
+     *
+     * @return mixed|void
+     */
+    public function onBeforeInvoke($actionInvoker)
+    {
+
+    }
+
+    /**
+     * @param \ManaPHP\ActionInvokerInterface $actionInvoker
+     *
+     * @return mixed|void
+     */
+    public function onAfterInvoke($actionInvoker)
+    {
+
+    }
+}
