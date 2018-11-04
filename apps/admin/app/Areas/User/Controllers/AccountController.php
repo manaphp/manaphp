6cd70a31b3a7a6ac7c427ec5fed02030fdab09dd
ManaPHP Mark <manaphp@qq.com>
2018-11-04 22:14:54 +0800

    Added      apps/admin/app/Areas/User/Views/Account/ChangePassword.sword Modified   apps/admin/app/Areas/User/Controllers/AccountController.php

--- apps/admin/app/Areas/User/Controllers/AccountController.php
+++ apps/admin/app/Areas/User/Controllers/AccountController.php
@@ -8,7 +8,12 @@ class AccountController extends Controller
 {
     public function getAcl()
     {
-        return ['register' => '*'];
+        return ['*' => 'user'];
+    }
+
+    public function captchaAction()
+    {
+        return $this->captcha->generate();
     }
 
     public function registerAction()
@@ -27,6 +32,32 @@ class AccountController extends Controller
             $admin->create();
 
             return 0;
+        }
+    }
+
+    public function changePasswordAction()
+    {
+        if ($this->request->isPost()) {
+            if (!$this->configure->debug) {
+                $this->captcha->verify();
+            }
+
+            try {
+                $old_password = $this->request->get('old_password', '*');
+                $new_password = $this->request->get('new_password', 'length:5-16');
+            } catch (\Exception $e) {
+                return $this->response->setJsonContent($e);
+            }
+            $admin = Admin::first(['admin_id' => $this->identity->getId()]);
+            if (!$admin || !$this->password->verify($old_password, $admin->password, $admin->salt)) {
+                return $this->response->setJsonError('旧密码不正确');
+            }
+
+            $admin->salt = $this->password->salt();
+            $admin->password = $this->password->hash($new_password, $admin->salt);
+            $admin->update();
+            $this->session->destroy();
+            return $this->response->setJsonContent(0);
         }
     }
 }
