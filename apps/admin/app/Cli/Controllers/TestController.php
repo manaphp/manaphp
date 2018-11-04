d6bd3b99b4c25a10bf7edfe6ea2aa169de222c16
ManaPHP Mark <manaphp@qq.com>
2018-11-04 10:14:34 +0800

    Added      apps/admin/app/Cli/Controllers/TestController.php

---
+++ apps/admin/app/Cli/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Cli\Controllers;
+
+use ManaPHP\Cli\Controller;
+
+/**
+ * Class TestController
+ * @package App\Cli\Controllers
+ */
+class TestController extends Controller
+{
+    public function defaultCommand()
+    {
+        $this->console->writeLn(date('Y-m-d H:i:s'));
+    }
+}
