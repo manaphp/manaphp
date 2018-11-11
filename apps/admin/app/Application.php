211625d001cb425b2900ed1325062fb5113f2015
ManaPHP Mark <manaphp@qq.com>
2018-11-11 22:16:57 +0800

    Modified   apps/admin/app/Application.php

--- apps/admin/app/Application.php
+++ apps/admin/app/Application.php
@@ -3,24 +3,5 @@ namespace App;
 
 class Application extends \ManaPHP\Mvc\Application
 {
-    /**
-     * @return void
-     */
-    public function main()
-    {
-        $this->dotenv->load();
-        $this->configure->load();
 
-        $this->registerServices();
-        $this->logger->debug(str_pad('', 80, '*'));
-        $this->view->setLayout();
-
-        try {
-            $this->handle();
-        } catch (\Exception $e) {
-            $this->handleException($e);
-        }
-
-        $this->response->send();
-    }
 }
