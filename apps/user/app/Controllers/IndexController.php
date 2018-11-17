741d38c050807a25f3b397bedc3b6f4a73b872d0
ManaPHP Mark <manaphp@qq.com>
2018-11-17 20:37:19 +0800

    Modified   apps/user/app/Controllers/IndexController.php Modified   apps/user/app/Views/Index/About.sword

--- apps/user/app/Controllers/IndexController.php
+++ apps/user/app/Controllers/IndexController.php
@@ -10,15 +10,10 @@ class IndexController extends ControllerBase
         return $this->response->redirect('about');
     }
 
-    /**
-     * @throws \ManaPHP\Curl\ConnectionException
-     */
     public function aboutAction()
     {
         $this->view->setVar('version', Version::get());
         $this->view->setVar('current_time', date('Y-m-d H:i:s'));
-
-        $this->view->setVar('baidu_time', date('Y-m-d H:i:s', strtotime($this->httpClient->get('https://www.baidu.com/')->getHeaders()['Date'])));
 
         $this->flash->error(date('Y-m-d H:i:s'));
     }
