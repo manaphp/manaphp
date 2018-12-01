98068302257176eb8a404a8e8d2390602b9bddc6
ManaPHP Mark <manaphp@qq.com>
2018-12-01 22:11:44 +0800

    Modified   ManaPHP/helpers.php

--- ManaPHP/helpers.php
+++ ManaPHP/helpers.php
@@ -922,12 +922,16 @@ if (!function_exists('array_field')) {
      */
     function array_field($input, $field_key)
     {
-        $values = [];
-        foreach ($input as $item) {
-            $values[] = is_array($item) ? $item[$field_key] : $item->$field_key;
-        }
+        if (PHP_MAJOR_VERSION >= 7) {
+            return array_column($input, $field_key);
+        } else {
+            $values = [];
+            foreach ($input as $item) {
+                $values[] = is_array($item) ? $item[$field_key] : $item->$field_key;
+            }
 
-        return $values;
+            return $values;
+        }
     }
 }
 
