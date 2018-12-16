e7e57e9ad5a12513c75a673a9732b304fef7c727
ManaPHP Mark <manaphp@qq.com>
2018-12-16 20:41:03 +0800

    Modified   ManaPHP/Service.php

--- ManaPHP/Service.php
+++ ManaPHP/Service.php
@@ -5,6 +5,25 @@ use ManaPHP\Logger\LogCategorizable;
 
 class Service extends Component implements LogCategorizable
 {
+    /**
+     * Service constructor.
+     *
+     * @param array $options
+     */
+    public function __construct($options = null)
+    {
+        if (is_array($options)) {
+            $vars = get_object_vars($this);
+            foreach ($options as $name => $value) {
+                $property = '_' . $name;
+
+                if (isset($vars[$property]) || array_key_exists($property, $vars)) {
+                    $this->$property = $value;
+                }
+            }
+        }
+    }
+
     public function categorizeLog()
     {
         return basename(str_replace('\\', '.', get_called_class()), 'Service');
