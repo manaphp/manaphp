d83e51789783258e98453db5f4b507c19509a5fe
ManaPHP Mark <manaphp@qq.com>
2019-02-17 10:07:21 +0800

    Modified   apps/user/docker/.env Modified   apps/user/docker/.env-dev Modified   apps/user/docker/.env-prod Modified   apps/user/docker/.env-stage Modified   apps/user/docker/.env-test Modified   apps/user/docker/docker-compose.yml Modified   apps/user/docker/nginx/conf/nginx.conf Modified   apps/user/manacli.php Modified   apps/user/public/index.php

--- apps/user/public/index.php
+++ apps/user/public/index.php
@@ -1,6 +1,11 @@
 <?php
 
-require __DIR__ . '/../../../ManaPHP/Loader.php';
+/** @noinspection PhpIncludeInspection */
+if (is_file(dirname(__DIR__) . '/vendor/manaphp/framework/Loader.php')) {
+    include dirname(__DIR__) . '/vendor/manaphp/framework/Loader.php';
+} else {
+    include __DIR__ . '/../../../ManaPHP/Loader.php';
+}
 
 $loader = new \ManaPHP\Loader();
 require dirname(__DIR__) . '/app/Application.php';
