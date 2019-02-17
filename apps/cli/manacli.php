8128543a1346d79ea74b2fe32643832c001e6b19
ManaPHP Mark <manaphp@qq.com>
2019-02-17 10:43:52 +0800

    Modified   apps/cli/manacli.php

--- apps/cli/manacli.php
+++ apps/cli/manacli.php
@@ -1,7 +1,12 @@
 #!/usr/bin/env php
 <?php
 
-require __DIR__ . '/../../ManaPHP/Loader.php';
+/** @noinspection PhpIncludeInspection */
+if (is_file(__DIR__ . '/vendor/manaphp/framework/Loader.php')) {
+    require __DIR__ . '/vendor/manaphp/framework/Loader.php';
+} else {
+    require __DIR__ . '/../../ManaPHP/Loader.php';
+}
 
 $loader = new \ManaPHP\Loader();
 require __DIR__ . '/app/Application.php';
