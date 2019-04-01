4eaf9b4069f612b1065232c8411488bc7f8a8eb0
ManaPHP Mark <manaphp@qq.com>
2019-04-01 10:35:04 +0800

    Add        ManaPHP/Cli/Controllers/SwordController.php

---
+++ ManaPHP/Cli/Controllers/SwordController.php
@@ -0,0 +1,74 @@
+<?php
+namespace ManaPHP\Cli\Controllers;
+
+use ManaPHP\Cli\Controller;
+
+/**
+ * Class SwordController
+ * @package ManaPHP\Cli\Controllers
+ *
+ * @property-read \ManaPHP\Renderer\Engine\Sword\Compiler $swordCompiler
+ */
+class SwordController extends Controller
+{
+    /**
+     * @param string $web
+     * @param string $asset
+     * @param bool   $replace
+     */
+    public function compileCommand($web = null, $asset = null, $replace = false)
+    {
+        if ($web) {
+            $this->alias->set('@web', $web);
+            if ($asset === null && $this->alias->get('@asset') === '') {
+                $this->alias->set('@asset', $web);
+            }
+        }
+
+        if ($asset) {
+            $this->alias->set('@asset', $asset);
+        }
+
+        $this->filesystem->dirDelete('@data/sword');
+        $this->console->writeLn('delete `@data/sword` directory success');
+
+        $ext = '.sword';
+
+        foreach ($this->filesystem->glob('@app/Views/*' . $ext) as $item) {
+            $this->_compile($item, $replace);
+        }
+
+        foreach ($this->filesystem->glob('@app/Views/*', GLOB_ONLYDIR) as $item) {
+            foreach ($this->filesystem->glob($item . '/*' . $ext) as $item2) {
+                $this->_compile($item2, $replace);
+            }
+        }
+
+        foreach ($this->filesystem->glob('@app/Areas/*', GLOB_ONLYDIR) as $item) {
+            foreach ($this->filesystem->glob($item . '/Views/*' . $ext) as $item2) {
+                $this->_compile($item2, $replace);
+            }
+
+            foreach ($this->filesystem->glob($item . '/Views/*/*' . $ext) as $item2) {
+                $this->_compile($item2, $replace);
+            }
+        }
+    }
+
+    /**
+     * @param string $file
+     * @param bool   $replace
+     */
+    public function _compile($file, $replace)
+    {
+        if ($replace) {
+            $compiled = str_replace('.sword', '.phtml', $file);
+        } else {
+            $compiled = str_replace($this->alias->resolve('@root'), '@data/sword', $file);
+        }
+
+        $this->swordCompiler->compileFile($file, $compiled);
+
+        $this->console->writeLn(['compiled `:file` file generated', 'file' => $compiled]);
+    }
+}
