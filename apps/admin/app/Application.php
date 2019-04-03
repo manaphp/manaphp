80e29d58cfe99456990ee712be26c07cb7c504e7
ManaPHP Mark <manaphp@qq.com>
2019-04-03 15:16:50 +0800

    Modified   apps/admin/app/Application.php

--- apps/admin/app/Application.php
+++ apps/admin/app/Application.php
@@ -3,5 +3,28 @@ namespace App;
 
 class Application extends \ManaPHP\Mvc\Application
 {
+    /**
+     * @var string
+     */
+    protected $_loginUrl = '/user/session/login';
 
+    public function authenticate()
+    {
+        $this->identity->authenticate();
+    }
+
+    public function authorize()
+    {
+        try {
+            $this->authorization->authorize();
+        } catch (\Exception $exception) {
+            if ($this->request->isAjax()) {
+                return $this->response->setJsonContent($exception);
+            } else {
+                $redirect = input('redirect', $this->request->getUrl());
+                $sep = (strpos($this->_loginUrl, '?') ? '&' : '?');
+                return $this->response->redirect(["{$this->_loginUrl}{$sep}redirect=$redirect"]);
+            }
+        }
+    }
 }
