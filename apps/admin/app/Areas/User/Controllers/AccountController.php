ba897840f4c027a9ee5267749497ddb501053f22
ManaPHP Mark <manaphp@qq.com>
2019-04-04 17:37:01 +0800

    Added      apps/admin/app/Areas/User/Controllers/PasswordController.php Rename     apps/admin/app/Areas/User/Views/Password/Change.sword (from apps/admin/app/Areas/User/Views/Account/ChangePassword.sword) Modified   apps/admin/app/Areas/User/Controllers/AccountController.php

--- apps/admin/app/Areas/User/Controllers/AccountController.php
+++ apps/admin/app/Areas/User/Controllers/AccountController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Areas\User\Controllers;
 
-use App\Areas\User\Services\ResetPasswordTokenService;
 use App\Models\Admin;
 use ManaPHP\Mvc\Controller;
 
@@ -9,7 +8,7 @@ class AccountController extends Controller
 {
     public function getAcl()
     {
-        return ['*' => 'user', 'resetPassword' => '*'];
+        return ['*' => 'user'];
     }
 
     public function captchaAction()
@@ -22,51 +21,6 @@ class AccountController extends Controller
         if ($this->request->isAjax()) {
             $this->captcha->verify();
             return Admin::createOrNull();
-        }
-    }
-
-    public function changePasswordAction()
-    {
-        if ($this->request->isPost()) {
-            if (!$this->configure->debug) {
-                $this->captcha->verify();
-            }
-
-            $admin = Admin::get($this->identity->getId());
-            if (!$admin->verifyPassword(input('old_password'))) {
-                return '旧密码不正确';
-            }
-
-            $admin->password = input('new_password');
-
-            $admin->update();
-            $this->session->destroy();
-            return 0;
-        }
-    }
-
-    public function resetPasswordAction(ResetPasswordTokenService $resetPasswordTokenService)
-    {
-        if ($this->request->isPost()) {
-            if (!$this->configure->debug) {
-                $this->captcha->verify();
-            }
-
-            $jwt = $resetPasswordTokenService->verify(input('token'));
-            if (!$jwt) {
-                return '已过期或无效';
-            }
-
-            $admin = Admin::firstOrFail(['admin_name' => $jwt['admin_name']]);
-
-            $admin->password = input('password');
-            $admin->update();
-            $this->session->destroy();
-            return $this->response->setJsonContent(0);
-        } else {
-            if ($this->identity->getRole() === 'admin') {
-                return $this->response->setJsonData($resetPasswordTokenService->generate(input('admin_name')));
-            }
         }
     }
 }
