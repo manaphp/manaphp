63fd341eaa05a75d1321e239213e78f1bc92247f
ManaPHP Mark <manaphp@qq.com>
2019-04-04 21:35:18 +0800

    Added      apps/admin/app/Areas/User/Views/Password/Forget.sword Added      apps/admin/app/Areas/User/Views/Password/Reset.sword Modified   apps/admin/app/Areas/User/Controllers/PasswordController.php

--- apps/admin/app/Areas/User/Controllers/PasswordController.php
+++ apps/admin/app/Areas/User/Controllers/PasswordController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Areas\User\Controllers;
 
-use App\Areas\User\Services\ResetPasswordTokenService;
 use App\Models\Admin;
 use ManaPHP\Mvc\Controller;
 
@@ -9,7 +8,47 @@ class PasswordController extends Controller
 {
     public function getAcl()
     {
-        return ['*' => 'user', 'reset' => '*'];
+        return ['*' => 'user', 'reset' => '*', 'forget' => '*', 'captcha' => '*'];
+    }
+
+    public function captchaAction()
+    {
+        return $this->captcha->generate();
+    }
+
+    public function forgetAction()
+    {
+        if ($this->request->isPost()) {
+            $user_name = input('user_name');
+            $email = input('email');
+
+            $admin = Admin::first(['admin_name' => $user_name]);
+            if (!$admin || $admin->email !== $email) {
+                return $this->response->setJsonError('账号不存在或账号与邮箱不匹配');
+            }
+            return $this->response->setJsonData(jwt_encode(['user_name' => $user_name, 'scope' => 'admin.user.password.forget'], 300), '重置密码链接已发到您的邮箱');
+        } else {
+            $this->view->setVar('redirect', input('redirect', $this->router->createUrl('/')));
+
+            return $this->view->setVar('user_name', $this->cookies->get('user_name'));
+        }
+    }
+
+    public function resetAction()
+    {
+        if ($this->request->isAjax()) {
+            $claims = jwt_decode(input('token'), 'admin.user.password.forget');
+
+            $user_name = $claims['user_name'];
+
+            $admin = Admin::firstOrFail(['admin_name' => $user_name]);
+            $admin->password = input('password');
+            $admin->update();
+
+            return $this->response->setJsonData([], '重置密码成功');
+        } else {
+            $claims = jwt_decode(input('token'), 'admin.user.password.forget');
+        }
     }
 
     public function changeAction()
@@ -29,31 +68,6 @@ class PasswordController extends Controller
             $admin->update();
             $this->session->destroy();
             return 0;
-        }
-    }
-
-    public function resetAction(ResetPasswordTokenService $resetPasswordTokenService)
-    {
-        if ($this->request->isPost()) {
-            if (!$this->configure->debug) {
-                $this->captcha->verify();
-            }
-
-            $jwt = $resetPasswordTokenService->verify(input('token'));
-            if (!$jwt) {
-                return '已过期或无效';
-            }
-
-            $admin = Admin::firstOrFail(['admin_name' => $jwt['admin_name']]);
-
-            $admin->password = input('password');
-            $admin->update();
-            $this->session->destroy();
-            return $this->response->setJsonContent(0);
-        } else {
-            if ($this->identity->getRole() === 'admin') {
-                return $this->response->setJsonData($resetPasswordTokenService->generate(input('admin_name')));
-            }
         }
     }
 }
