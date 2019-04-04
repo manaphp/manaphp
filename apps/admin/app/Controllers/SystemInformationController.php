c05046e12d81c5450a423e6ded85f407dbdaffce
ManaPHP Mark <manaphp@qq.com>
2019-04-04 10:53:35 +0800

    Modified   apps/admin/app/Controllers/SystemInformationController.php

--- apps/admin/app/Controllers/SystemInformationController.php
+++ apps/admin/app/Controllers/SystemInformationController.php
@@ -8,11 +8,14 @@ class SystemInformationController extends Controller
 {
     public function indexAction()
     {
+        $globals = $this->request->getGlobals();
+
         $data = [];
 
         $data['framework_version'] = Version::get();
         $data['php_version'] = PHP_VERSION;
         $data['sapi'] = PHP_SAPI;
+
 
         if (function_exists('apache_get_version')) {
             $data['apache_version'] = apache_get_version();
@@ -22,8 +25,8 @@ class SystemInformationController extends Controller
         $data['operating_system'] = php_uname();
         $data['system_time'] = date('Y-m-d H:i:s');
         $data['loaded_ini'] = '';
-        $data['server_ip'] = $_SERVER['SERVER_ADDR'];
-        $data['client_ip'] = $_SERVER['REMOTE_ADDR'];
+        $data['server_ip'] = $globals->_SERVER['SERVER_ADDR'];
+        $data['client_ip'] = $globals->_SERVER['REMOTE_ADDR'];
         $data['upload_max_filesize'] = ini_get('upload_max_filesize');
         $data['post_max_size'] = ini_get('post_max_size');
         $data['loaded_ini'] = php_ini_loaded_file();
