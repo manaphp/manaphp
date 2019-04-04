a9b204796f3f01eb936a6694043cd863b38dfb62
ManaPHP Mark <manaphp@qq.com>
2019-04-04 16:00:52 +0800

    Modified   apps/admin/app/Areas/User/Views/ActionLog.sword Modified   apps/admin/app/Areas/User/Views/LoginLog.sword Modified   apps/admin/public/assets/js/app.js

--- apps/admin/public/assets/js/app.js
+++ apps/admin/public/assets/js/app.js
@@ -89,3 +89,116 @@ Vue.component('pager', {
         '                   @size-change="request.size=$event"\n' +
         '                   :total="response.count" layout="sizes,total, prev, pager, next"></el-pagination>\n',
 });
+
+Vue.component('date-picker', {
+    props: ['value'],
+    template: '<el-date-picker v-model="value" type="daterange" start-placeholder="开始日期"\n ' +
+        '                            end-placeholder="结束日期" value-format="yyyy-MM-dd" size="small"\n ' +
+        ' :picker-options="pickerOptions" @change="change"' +
+        '></el-date-picker>',
+    methods: {
+        change: function (value) {
+            this.$emit('input', value);
+        }
+    },
+    data: function () {
+        return {
+            pickerOptions: {
+                shortcuts: [
+                    {
+                        text: '今天',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime());
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '昨天',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24);
+                            end.setDate(end.getDate() - 1);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '最近三天',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 3);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '最近一周',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '最近一个月',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '最近三个月',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '最近一年',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 365);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '本周',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setDate(start.getDate() - start.getDay() + 1);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '本月',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setDate(1);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: '本季',
+                        onClick: function (picker) {
+                            var end = new Date();
+                            var start = new Date();
+                            start.setMonth(parseInt(start.getMonth() / 3) * 3);
+                            start.setDate(1);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }
+                ]
+            }
+        }
+    }
+});
