e2a0df69a286674c43f3f8a9b4197186916df460
ManaPHP Mark <manaphp@qq.com>
2019-04-27 22:23:27 +0800

    Modified   apps/api/app/Models/City.php Modified   apps/cli/app/Models/City.php Modified   apps/user/app/Models/City.php

--- apps/cli/app/Models/City.php
+++ apps/cli/app/Models/City.php
@@ -11,4 +11,14 @@ class City extends \ManaPHP\Db\Model
     public $city;
     public $country_id;
     public $last_update;
+
+    public static function sample()
+    {
+        return [
+            'city_id' => 1,
+            'city' => 'beijing',
+            'country_id' => 1,
+            'last_update' => '2019-01-02 11:12:13'
+        ];
+    }
 }
