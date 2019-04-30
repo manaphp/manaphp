00d6adb24dcb20e3c183ada35bd3ae1e5b15dad1
ManaPHP Mark <manaphp@qq.com>
2019-04-30 22:11:29 +0800

    Rename     apps/admin/app/Areas/User/Views/ActionLog/Index.sword (from apps/admin/app/Areas/User/Views/ActionLog.sword) Added      apps/admin/app/Areas/User/Views/ActionLog/Latest.sword Modified   apps/admin/app/Areas/User/Controllers/ActionLogController.php Modified   apps/admin/app/Views/Layouts/Default.sword

--- apps/admin/app/Areas/User/Controllers/ActionLogController.php
+++ apps/admin/app/Areas/User/Controllers/ActionLogController.php
@@ -6,6 +6,11 @@ use ManaPHP\Mvc\Controller;
 
 class ActionLogController extends Controller
 {
+    public function getAcl()
+    {
+        return ['latest' => 'user', 'detail' => '@index', 'detailSelf' => 'user'];
+    }
+
     public function indexAction()
     {
         return $this->request->isAjax()
@@ -20,5 +25,27 @@ class ActionLogController extends Controller
     public function detailAction()
     {
         return AdminActionLog::firstOrNull();
+    }
+
+    public function detailSelfAction()
+    {
+        $log = AdminActionLog::firstOrNull();
+        if ($log->user_id != $this->identity->getId()) {
+            return '没有权限';
+        }
+
+        return $log;
+    }
+
+    public function latestAction()
+    {
+        return $this->request->isAjax()
+            ? AdminActionLog::query()
+                ->select(['id', 'client_ip', 'method', 'path', 'url', 'created_time'])
+                ->where('user_id', $this->identity->getId())
+                ->whereSearch(['path', 'client_ip', 'created_time@='])
+                ->orderBy(['id' => SORT_DESC])
+                ->paginate()
+            : null;
     }
 }
