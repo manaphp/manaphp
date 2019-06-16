0dc922e00370836211ad9e0fe02c30bee58f5159
ManaPHP Mark <manaphp@qq.com>
2019-06-16 21:10:41 +0800

    Modified   apps/ws/app/Application.php

--- apps/ws/app/Application.php
+++ apps/ws/app/Application.php
@@ -5,6 +5,17 @@ use App\Processes\PusherProcess;
 
 class Application extends \ManaPHP\WebSocket\Application
 {
+    public function authenticate()
+    {
+//        $this->wsPusher->pushToId(2, 'abczc', 'admin');
+//        $this->wsPusher->pushToName('mark', 'mark', 'admin');
+//        $this->wsPusher->pushToRole('admin', 'abc', 'admin');
+//        $this->wsPusher->pushToAll('abc', 'admin');
+//        $this->wsPusher->broadcast(date('Y-m-d H:i:s'),'admin');
+//
+        $this->identity->setClaims(['user_id' => 1, 'user_name' => 'mark', 'role' => 'admin']);
+    }
+
     public function getProcesses()
     {
         return ['pusherProcess' => ['class' => PusherProcess::class, 'endpoint' => 'admin']];
