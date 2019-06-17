0522fd34f2f4595339240a5db5bfd75d717236eb
ManaPHP Mark <manaphp@qq.com>
2019-06-17 20:45:28 +0800

    Modified   apps/ws/config/app.php

--- apps/ws/config/app.php
+++ apps/ws/config/app.php
@@ -1,24 +1,19 @@
 <?php
 
 return [
+    'id' => 'cli',
+    'env' => env('APP_ENV', 'prod'),
     'debug' => env('APP_DEBUG', false),
     'version' => '1.1.1',
     'timezone' => 'PRC',
-    'master_key' => '',
+    'master_key' => env('MASTER_KEY'),
     'params' => ['manaphp_brand_show' => 1],
     'aliases' => [
-        '@xxx' => '@root/xvdfd'
     ],
     'components' => [
         'wsServer' => ['worker_num' => 2],
-        'db' => 'mysql://root:123456@localhost/manaphp?charset=utf8',
-        'redis' => 'redis://localhost:6379/1?timeout=2&retry_interval=0&auth=&persistent=0',
-        'mongodb' => 'mongodb://127.0.0.1/manaphp_unit_test',
-        'logger' => ['level' => 'debug'],
+        'db' => [env('DB_URL')],
+        'redis' => [env('REDIS_URL')],
+        'logger' => ['level' => env('LOGGER_LEVEL', 'info')],
     ],
-    'services' => [],
-    'listeners' => [],
-    'plugins' => [
-        //  'debugger'
-    ]
 ];
