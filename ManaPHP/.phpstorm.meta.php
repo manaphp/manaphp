a751183d92994363d2b6e8b5a071a5c35c6c7eec
ManaPHP Mark <manaphp@qq.com>
2019-07-12 21:04:16 +0800

    Modified   ManaPHP/.phpstorm.meta.php

--- ManaPHP/.phpstorm.meta.php
+++ ManaPHP/.phpstorm.meta.php
@@ -126,7 +126,7 @@ namespace PHPSTORM_META {
     expectedArguments(\ManaPHP\Http\RequestInterface::hasServer(), 0, array_keys($_SERVER)[$i]);
 
     expectedArguments(\ManaPHP\Http\ResponseInterface::setJsonContent(), 0, ['code' => 0, 'message' => '', 'data' => []]);
-    expectedReturnValues(\ManaPHP\Mvc\Controller::getAcl(), 0,
+    expectedReturnValues(\ManaPHP\Mvc\Controller::getAcl(),
         ['list' => '@index', 'detail' => '@index', 'create' => '@index', 'delete' => '@index', 'edit' => '@index']);
 
     registerArgumentsSet('wsPusherEndpoint', 'admin', 'user');
