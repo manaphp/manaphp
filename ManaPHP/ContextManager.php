70fa34e9cf0c2d780c6077f5278dc49cad42c983
ManaPHP Mark <manaphp@qq.com>
2019-07-25 20:46:12 +0800

    Modified   ManaPHP/ContextManager.php

--- ManaPHP/ContextManager.php
+++ ManaPHP/ContextManager.php
@@ -1,6 +1,7 @@
 <?php
 namespace ManaPHP;
 
+use ManaPHP\Coroutine\Context\Inseparable;
 use Swoole\Coroutine;
 
 class ContextManager
@@ -21,7 +22,19 @@ class ContextManager
 
         $oid = spl_object_id($object);
         if (!isset(self::$_contexts[$cid][$oid])) {
-            return self::$_contexts[$cid][$oid] = $object->createContext();
+            if ($root_cid = self::$_contexts[$cid]['root_cid'] ?? null) {
+                if ($context = self::$_contexts[$root_cid][$oid] ?? null) {
+                    return self::$_contexts[$cid][$oid] = $context instanceof Inseparable ? $object->createContext() : $context;
+                } else {
+                    $context = self::$_contexts[$cid][$oid] = $object->createContext();
+                    if (!$context instanceof Inseparable) {
+                        self::$_contexts[$root_cid][$oid] = $context;
+                    }
+                    return $context;
+                }
+            } else {
+                return self::$_contexts[$cid][$oid] = $object->createContext();
+            }
         } else {
             return self::$_contexts[$cid][$oid];
         }
