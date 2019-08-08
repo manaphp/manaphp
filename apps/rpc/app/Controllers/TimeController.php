d0392db55aed881e8d2d906e013742590a0287ce
ManaPHP Mark <manaphp@qq.com>
2019-08-08 10:34:41 +0800

    Modified   apps/rpc/app/Controllers/TimeController.php

--- apps/rpc/app/Controllers/TimeController.php
+++ apps/rpc/app/Controllers/TimeController.php
@@ -18,26 +18,6 @@ class TimeController extends Controller
 
     public function currentAction()
     {
-        $data = [];
-        $data['current_time'] = date('Y-m-d H:i:s');
-//        $data['memory_usage'] = round(memory_get_usage(false) / 1024) . 'KB';
-//        $data['process_time'] = sprintf('%.03f', microtime(true) - $this->request->getServer('REQUEST_TIME_FLOAT'));
-//        $data['files'] = @get_included_files();
-        return $this->response->setJsonContent($data);
-    }
-
-    public function timestampAction()
-    {
-        $access_token = $this->request->getToken();
-        if ($access_token !== 'manaphp') {
-            return $this->response->setJsonContent($access_token === '' ? 'access_token is missing.' : 'access_token is wrong.');
-        }
-
-        $time = time();
-        $data = [];
-        $data['timestamp'] = $time;
-        $data['time_human'] = date('Y-m-d H:i:s', $time);
-
-        return $this->response->setJsonContent($data);
+        return ['code' => 0, 'message' => '', 'data' => time()];
     }
 }
