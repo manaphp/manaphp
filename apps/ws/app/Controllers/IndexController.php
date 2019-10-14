333253411c64aed7b6ec780315ab92b68a42f456
ManaPHP Mark <manaphp@qq.com>
2019-10-14 17:41:28 +0800

    Modified   apps/ws/app/Controllers/IndexController.php

--- apps/ws/app/Controllers/IndexController.php
+++ apps/ws/app/Controllers/IndexController.php
@@ -6,9 +6,26 @@ use ManaPHP\WebSocket\Controller;
 
 class IndexController extends Controller
 {
-    public function openAction()
+    public function openAction($fd)
     {
+//        $data = [];
+//        $data['admin_id'] = $this->identity->getId();
+//        $data['admin_name'] = $this->identity->getName();
+//        $data['role'] = $this->identity->getRole();
+//
+//        $token = jwt_encode($data, $ttl, 'pusher.admin');
+
         $token = $this->request->getToken();
         $this->identity->setClaims(jwt_decode($token, 'pusher.admin'));
+    }
+
+    public function closeAction($fd)
+    {
+
+    }
+
+    public function messageAction($data)
+    {
+
     }
 }
