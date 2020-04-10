a3913cd8ded93c95921cff5c57c8560398825d6c
ManaPHP Mark <manaphp@qq.com>
2020-04-10 09:34:18 +0800

    Modified   apps/admin/install.sql

--- apps/admin/install.sql
+++ apps/admin/install.sql
@@ -23,14 +23,15 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(32) CHARACTER SET ascii NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '0',
-  `type` tinyint(4) NOT NULL DEFAULT '1',
-  `tag` int(11) NOT NULL DEFAULT '0',
+  `status` tinyint(4) NOT NULL DEFAULT 0,
+  `type` tinyint(4) NOT NULL DEFAULT 1,
+  `tag` int(11) NOT NULL DEFAULT 0,
   `email` varchar(64) CHARACTER SET ascii NOT NULL,
   `salt` char(16) CHARACTER SET ascii NOT NULL,
   `password` char(32) CHARACTER SET ascii NOT NULL,
+  `white_ip` varchar(64) NOT NULL DEFAULT '*',
   `login_ip` char(15) CHARACTER SET ascii NOT NULL DEFAULT '',
-  `login_time` int(11) NOT NULL DEFAULT '0',
+  `login_time` int(11) NOT NULL DEFAULT 0,
   `session_id` varchar(64) NOT NULL DEFAULT '',
   `creator_name` varchar(32) CHARACTER SET ascii NOT NULL,
   `updator_name` varchar(32) CHARACTER SET ascii NOT NULL,
@@ -39,7 +40,7 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`admin_id`),
   UNIQUE KEY `admin_name` (`admin_name`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `admin` */
 
@@ -54,17 +55,18 @@ CREATE TABLE `admin_action_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` int(11) NOT NULL,
   `admin_name` varchar(32) CHARACTER SET ascii NOT NULL,
-  `client_ip` char(15) CHARACTER SET ascii NOT NULL,
-  `client_udid` char(16) CHARACTER SET ascii NOT NULL DEFAULT '',
-  `path` varchar(64) CHARACTER SET ascii NOT NULL,
   `method` varchar(15) CHARACTER SET ascii NOT NULL,
+  `path` varchar(64) CHARACTER SET ascii NOT NULL,
+  `tag` int(11) NOT NULL DEFAULT 0,
   `url` varchar(128) NOT NULL,
   `data` text NOT NULL,
+  `client_ip` char(15) CHARACTER SET ascii NOT NULL,
+  `client_udid` char(16) CHARACTER SET ascii NOT NULL DEFAULT '',
   `created_time` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`admin_id`),
   KEY `path` (`path`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `admin_action_log` */
 
@@ -78,11 +80,11 @@ CREATE TABLE `admin_login_log` (
   `admin_name` char(16) CHARACTER SET ascii NOT NULL,
   `client_ip` char(15) CHARACTER SET ascii NOT NULL,
   `client_udid` char(16) CHARACTER SET ascii NOT NULL,
-  `user_agent` char(128) CHARACTER SET ascii NOT NULL,
+  `user_agent` char(255) CHARACTER SET ascii NOT NULL,
   `created_time` int(11) NOT NULL,
   PRIMARY KEY (`login_id`),
   KEY `admin_id` (`admin_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `admin_login_log` */
 
@@ -97,7 +99,7 @@ CREATE TABLE `bos_bucket` (
   `created_time` int(11) NOT NULL,
   PRIMARY KEY (`bucket_id`),
   UNIQUE KEY `bucket_name` (`bucket_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `bos_bucket` */
 
@@ -116,8 +118,8 @@ CREATE TABLE `bos_object` (
   `original_name` varchar(128) NOT NULL,
   `mime_type` varchar(64) CHARACTER SET ascii NOT NULL,
   `extension` varchar(16) CHARACTER SET ascii NOT NULL,
-  `width` int(11) NOT NULL DEFAULT '0',
-  `height` int(11) NOT NULL DEFAULT '0',
+  `width` int(11) NOT NULL DEFAULT 0,
+  `height` int(11) NOT NULL DEFAULT 0,
   `size` int(11) NOT NULL,
   `md5` char(32) CHARACTER SET ascii NOT NULL,
   `ip` char(15) CHARACTER SET ascii NOT NULL,
@@ -128,9 +130,20 @@ CREATE TABLE `bos_object` (
   KEY `extension` (`extension`),
   KEY `key` (`key`),
   KEY `bucket_id` (`bucket_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-/*Data for the table `bos_object` */
+/*Table structure for table `dotenv_log` */
+
+DROP TABLE IF EXISTS `dotenv_log`;
+
+CREATE TABLE `dotenv_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_id` varchar(32) CHARACTER SET ascii NOT NULL,
+  `env` text NOT NULL,
+  `created_date` int(11) NOT NULL,
+  `created_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Table structure for table `menu_group` */
 
@@ -147,7 +160,7 @@ CREATE TABLE `menu_group` (
   `updated_time` int(11) NOT NULL,
   PRIMARY KEY (`group_id`),
   UNIQUE KEY `group_name` (`group_name`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `menu_group` */
 
@@ -176,14 +189,14 @@ CREATE TABLE `menu_item` (
   `updated_time` int(11) NOT NULL,
   PRIMARY KEY (`item_id`),
   UNIQUE KEY `group_id` (`group_id`,`url`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `menu_item` */
 
 insert  into `menu_item`(`item_id`,`item_name`,`group_id`,`display_order`,`url`,`icon`,`creator_name`,`updator_name`,`created_time`,`updated_time`) values 
 (8,'角色权限',3,0,'/rbac/role_permission','el-icon-arrow-right','admin','admin',0,0),
 (4,'用户',3,3,'/rbac/admin','el-icon-arrow-right','admin','admin',0,0),
-(9,'系统信息',5,0,'/system_information','el-icon-arrow-right','admin','',0,0),
+(9,'系统信息',5,0,'/system/information','el-icon-arrow-right','admin','',0,0),
 (5,'角色',3,0,'/rbac/role','el-icon-arrow-right','admin','admin',0,0),
 (6,'权限',3,0,'/rbac/permission','el-icon-arrow-right','admin','admin',0,0),
 (7,'用户角色',3,0,'/rbac/admin_role','el-icon-arrow-right','admin','admin',0,0),
@@ -205,7 +218,7 @@ CREATE TABLE `metadata_constant` (
   `id` varchar(32) NOT NULL,
   `constants` tinytext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `metadata_constant` */
 
@@ -223,7 +236,7 @@ CREATE TABLE `rbac_admin_role` (
   `created_time` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id_role_id` (`admin_id`,`role_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `rbac_admin_role` */
 
@@ -239,7 +252,7 @@ CREATE TABLE `rbac_permission` (
   `updated_time` int(11) NOT NULL,
   PRIMARY KEY (`permission_id`),
   UNIQUE KEY `path` (`path`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `rbac_permission` */
 
@@ -251,7 +264,7 @@ CREATE TABLE `rbac_role` (
   `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `role_name` varchar(64) NOT NULL,
   `display_name` varchar(64) NOT NULL,
-  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  `enabled` tinyint(4) NOT NULL DEFAULT 1,
   `permissions` text CHARACTER SET ascii NOT NULL,
   `creator_name` varchar(32) CHARACTER SET ascii NOT NULL,
   `updator_name` varchar(32) CHARACTER SET ascii NOT NULL,
@@ -260,14 +273,14 @@ CREATE TABLE `rbac_role` (
   PRIMARY KEY (`role_id`),
   UNIQUE KEY `role_name` (`role_name`),
   UNIQUE KEY `display_name` (`display_name`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `rbac_role` */
 
 insert  into `rbac_role`(`role_id`,`role_name`,`display_name`,`enabled`,`permissions`,`creator_name`,`updator_name`,`created_time`,`updated_time`) values 
-(3,'menu','菜单管理员',1,'','admin','admin',0,0),
 (1,'admin','超级管理员',1,'','admin','admin',0,0),
-(2,'rbac','权限管理员',1,'','admin','admin',0,0);
+(2,'rbac','权限管理员',1,'','admin','admin',0,0),
+(3,'menu','菜单管理员',1,'','admin','admin',0,0);
 
 /*Table structure for table `rbac_role_permission` */
 
@@ -281,7 +294,7 @@ CREATE TABLE `rbac_role_permission` (
   `created_time` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `permission_id_role_id` (`permission_id`,`role_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `rbac_role_permission` */
 
@@ -293,7 +306,7 @@ CREATE TABLE `test` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `balance` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `test` */
 
