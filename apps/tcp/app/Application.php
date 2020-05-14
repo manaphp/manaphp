dd665339088ce6a14200101b36085a306a76f0e0
ManaPHP Mark <manaphp@qq.com>
2020-05-14 12:08:51 +0800

    Modified   apps/tcp/app/Application.php

--- apps/tcp/app/Application.php
+++ apps/tcp/app/Application.php
@@ -4,5 +4,13 @@ namespace App;
 
 class Application extends \ManaPHP\Socket\Application
 {
+    public function onReceive($fd, $data)
+    {
+        parent::onReceive($fd, substr($data, 4));
+    }
 
+    public function send($fd, $data)
+    {
+        return parent::send($fd, pack('L', strlen($data)) . $data);
+    }
 }
