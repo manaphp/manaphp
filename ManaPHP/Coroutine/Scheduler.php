06e9a80d858392ff3b3c281522a046991dddaee2
ManaPHP Mark <manaphp@qq.com>
2020-10-05 23:16:24 +0800

    Modified   ManaPHP/Coroutine/Scheduler.php

--- ManaPHP/Coroutine/Scheduler.php
+++ ManaPHP/Coroutine/Scheduler.php
@@ -59,6 +59,7 @@ class Scheduler extends Component implements SchedulerInterface
 
             foreach ($this->_tasks as $id => $task) {
                 $returns[$id] = null;
+                /** @noinspection PhpMethodParametersCountMismatchInspection */
                 Coroutine::create([$this, 'routine'], $id, $channel, $task);
             }
 
