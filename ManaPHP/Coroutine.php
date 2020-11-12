df53492c37b23b8eb8debc347fb9581376a1ea6e
ManaPHP Mark <manaphp@qq.com>
2020-11-12 08:46:11 +0800

    Modified   ManaPHP/Coroutine.php

--- ManaPHP/Coroutine.php
+++ ManaPHP/Coroutine.php
@@ -9,7 +9,9 @@ class Coroutine
     public static function getBacktrace($options, $limit = 0)
     {
         if (MANAPHP_COROUTINE_ENABLED) {
-            return SwooleCoroutine::getBackTrace(0, $options, $limit);
+            $traces = SwooleCoroutine::getBackTrace(0, $options, $limit + 1);
+            array_shift($traces);
+            return $traces;
         } else {
             return debug_backtrace($options, $limit);
         }
