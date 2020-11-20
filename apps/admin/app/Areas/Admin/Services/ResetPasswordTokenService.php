3ec4599700876b804c0781541ae2170e834fcfa3
ManaPHP Mark <manaphp@qq.com>
2020-11-20 08:17:38 +0800

    Modified   apps/admin/app/Areas/Admin/Services/ResetPasswordTokenService.php

--- apps/admin/app/Areas/Admin/Services/ResetPasswordTokenService.php
+++ apps/admin/app/Areas/Admin/Services/ResetPasswordTokenService.php
@@ -4,23 +4,9 @@ namespace App\Areas\Admin\Services;
 
 use App\Models\Admin;
 use App\Services\Service;
-use ManaPHP\Token\Jwt;
 
 class ResetPasswordTokenService extends Service
 {
-    const KEY_SCOPE = 'admin:user:account:reset_password';
-    const TTL = 1800;
-
-    /**
-     * @var Jwt
-     */
-    protected $_jwt;
-
-    public function __construct()
-    {
-        $this->_jwt = new Jwt(['key' => $this->crypt->getDerivedKey(self::KEY_SCOPE), 'ttl' => self::TTL]);
-    }
-
     /**
      * @param $admin_name
      *
@@ -29,8 +15,7 @@ class ResetPasswordTokenService extends Service
     public function generate($admin_name)
     {
         $admin = Admin::firstOrFail(['admin_name' => $admin_name]);
-        $data = ['admin_name' => $admin->admin_name];
-        return $this->_jwt->encode($data);
+        return jwt_encode(['admin_name' => $admin->admin_name], 1800, 'admin.reset_password');
     }
 
     /**
@@ -41,7 +26,7 @@ class ResetPasswordTokenService extends Service
     public function verify($token)
     {
         try {
-            return $this->_jwt->decode($token);
+            return jwt_decode($token, 'admin.reset_password');
         } catch (\Exception $exception) {
             return false;
         }
