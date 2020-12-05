373ef2325240a06bf0b748e113b2023e35dd2efa
ManaPHP Mark <manaphp@qq.com>
2020-12-05 09:02:11 +0800

    Modified   ManaPHP/Data/Db/Table.php

--- ManaPHP/Data/Db/Table.php
+++ ManaPHP/Data/Db/Table.php
@@ -23,4 +23,138 @@ class Table extends \ManaPHP\Data\Table
 
         return static::sample()->getShared($db);
     }
+
+    /**
+     * @param array $record
+     * @param bool  $fetchInsertId
+     *
+     * @return int|string|null
+     * @throws \ManaPHP\Data\Db\Exception
+     */
+    public static function insert($record, $fetchInsertId = false)
+    {
+        $sample = static::sample();
+
+        list($db, $table) = $sample->getUniqueShard($record);
+
+        /** @var \ManaPHP\Data\DbInterface $db */
+        $db = static::sample()->getShared($db);
+
+        return $db->insert($table, $record, $fetchInsertId);
+    }
+
+    /**
+     * @param string $sql
+     * @param array  $bind
+     *
+     * @return int
+     */
+    public static function insertBySql($sql, $bind = [])
+    {
+        $sample = static::sample();
+
+        list($db, $table) = $sample->getUniqueShard($bind);
+
+        /** @var \ManaPHP\Data\DbInterface $db */
+        $db = static::sample()->getShared($db);
+
+        return $db->insertBySql($table, $sql, $bind);
+    }
+
+    /**
+     * Deletes data from a table using custom SQL syntax
+     *
+     * @param string|array $conditions
+     * @param array        $bind
+     *
+     * @return int
+     */
+    public static function delete($conditions, $bind = [])
+    {
+        $shards = static::sample()->getMultipleShards($bind);
+
+        $affected_count = 0;
+        foreach ($shards as $db => $tables) {
+            /** @var \ManaPHP\Data\DbInterface $db */
+            $db = static::sample()->getShared($db);
+
+            foreach ($tables as $table) {
+                $affected_count += $db->delete($table, $conditions, $bind);
+            }
+        }
+
+        return $affected_count;
+    }
+
+    /**
+     * @param string $sql
+     * @param array  $bind
+     *
+     * @return int
+     */
+    public static function deleteBySql($sql, $bind = [])
+    {
+        $shards = static::sample()->getMultipleShards($bind);
+
+        $affected_count = 0;
+        foreach ($shards as $db => $tables) {
+            /** @var \ManaPHP\Data\DbInterface $db */
+            $db = static::sample()->getShared($db);
+
+            foreach ($tables as $table) {
+                $affected_count += $db->deleteBySql($table, $sql, $bind);
+            }
+        }
+
+        return $affected_count;
+    }
+
+    /**
+     * Updates data on a table using custom SQL syntax
+     *
+     * @param array        $fieldValues
+     * @param string|array $conditions
+     * @param array        $bind
+     *
+     * @return    int
+     */
+    public static function update($fieldValues, $conditions, $bind = [])
+    {
+        $shards = static::sample()->getMultipleShards($bind);
+
+        $affected_count = 0;
+        foreach ($shards as $db => $tables) {
+            /** @var \ManaPHP\Data\DbInterface $db */
+            $db = static::sample()->getShared($db);
+
+            foreach ($tables as $table) {
+                $affected_count += $db->update($table, $fieldValues, $conditions, $bind);
+            }
+        }
+
+        return $affected_count;
+    }
+
+    /**
+     * @param string $sql
+     * @param array  $bind
+     *
+     * @return int
+     */
+    public static function updateBySql($sql, $bind = [])
+    {
+        $shards = static::sample()->getMultipleShards($bind);
+
+        $affected_count = 0;
+        foreach ($shards as $db => $tables) {
+            /** @var \ManaPHP\Data\DbInterface $db */
+            $db = static::sample()->getShared($db);
+
+            foreach ($tables as $table) {
+                $affected_count += $db->updateBySql($table, $sql, $bind);
+            }
+        }
+
+        return $affected_count;
+    }
 }
