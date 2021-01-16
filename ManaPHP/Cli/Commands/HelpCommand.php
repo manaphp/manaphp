1b0beb55c6722d7cb0caeacaed23507132df6457
ManaPHP Mark <manaphp@qq.com>
2021-01-16 10:48:33 +0800

    Modified   ManaPHP/Cli/Commands/HelpCommand.php

--- ManaPHP/Cli/Commands/HelpCommand.php
+++ ManaPHP/Cli/Commands/HelpCommand.php
@@ -127,6 +127,104 @@ class HelpCommand extends Command
     }
 
     /**
+     * @param ReflectionMethod $rm
+     * @param string           $method
+     *
+     * @throws \ManaPHP\Exception\JsonException
+     */
+    protected function _commandHelp($rm, $method)
+    {
+        $lines = [];
+        foreach (preg_split('#[\r\n]+#', $rm->getDocComment()) as $line) {
+            $lines[] = trim($line, "\t /*\r\n");
+        }
+
+        $description = '';
+        foreach ($lines as $line) {
+            if (!$line) {
+                continue;
+            }
+
+            if ($line[0] !== '@') {
+                $description = $line;
+            }
+            break;
+        }
+
+        $method_name = str_pad(basename($method, 'Action'), 10);
+        $colored_action = $this->console->colorize($method_name, Console::FC_YELLOW) . ' ' . $description;
+        $this->console->writeLn($colored_action);
+
+        $defaultValues = [];
+        foreach ($rm->getParameters() as $parameter) {
+            if ($parameter->isDefaultValueAvailable()) {
+                $defaultValues[$parameter->getName()] = $parameter->getDefaultValue();
+            }
+        }
+
+        $params = [];
+        foreach ($lines as $line) {
+            if (!str_contains($line, '@param')) {
+                continue;
+            }
+
+            $parts = preg_split('#\s+#', $line, 4);
+            if (count($parts) < 3 || $parts[0] !== '@param') {
+                continue;
+            }
+            $name = substr($parts[2], 1);
+            $type = $parts[1];
+
+            if (isset($defaultValues[$name])) {
+                if ($type === 'bool' || $type === 'boolean') {
+                    $defaultValues[$name] = $defaultValues[$name] ? 'YES' : 'NO';
+                } elseif ($type === 'int' || $type === 'integer') {
+                    $defaultValues[$name] = (int)$defaultValues[$name];
+                } elseif ($type === 'float' || $type === 'double') {
+                    $defaultValues[$name] = (float)$defaultValues[$name];
+                } elseif ($type === 'string') {
+                    $defaultValues[$name] = json_stringify($defaultValues[$name]);
+                } elseif ($type === 'array') {
+                    $defaultValues[$name] = json_stringify($defaultValues[$name]);
+                }
+            }
+
+            $params[$name] = isset($parts[3]) ? trim($parts[3]) : '';
+        }
+
+        if ($params) {
+            $shortNames = [];
+            foreach ($params as $name => $description) {
+                $short = $name[0];
+                if (isset($shortNames[$short])) {
+                    $shortNames[$short] = false;
+                } else {
+                    $shortNames[$short] = $name;
+                }
+            }
+            $shortNames = array_flip(array_filter($shortNames));
+
+            $width = 1;
+            foreach ($params as $name => $description) {
+                $width = max($width, strlen($name) + 2 + (isset($shortNames[$name]) ? 4 : 0));
+            }
+            $this->console->writeLn('  Options:');
+
+            foreach ($params as $name => $value) {
+                $option = '--' . $name;
+                if (isset($shortNames[$name])) {
+                    $option .= ', -' . $shortNames[$name];
+                }
+
+                $ov = $this->console->colorize($option, Console::FC_CYAN, $width);
+                $vv = $value ? "  $value" : '';
+                $dv = isset($defaultValues[$name]) ? " (default: $defaultValues[$name])" : '';
+                $this->console->writeLn(['    %s %s %s', $ov, $vv, $dv]);
+            }
+        }
+    }
+
+    /**
      * list command
      *
      * @param string $command
@@ -150,93 +248,11 @@ class HelpCommand extends Command
                 continue;
             }
 
-            $lines = [];
-            foreach (preg_split('#[\r\n]+#', $rm->getDocComment()) as $line) {
-                $lines[] = trim($line, "\t /*\r\n");
-            }
-
-            $description = '';
-            foreach ($lines as $line) {
-                if (!$line) {
-                    continue;
-                }
-
-                if ($line[0] !== '@') {
-                    $description = $line;
-                }
-                break;
-            }
-
-            $method_name = str_pad(basename($method, 'Action'), 10);
-            $colored_action = $this->console->colorize($method_name, Console::FC_YELLOW) . ' ' . $description;
-            $this->console->writeLn($colored_action);
-
-            $defaultValues = [];
-            foreach ($rm->getParameters() as $parameter) {
-                if ($parameter->isDefaultValueAvailable()) {
-                    $defaultValues[$parameter->getName()] = $parameter->getDefaultValue();
-                }
-            }
-
-            $params = [];
-            foreach ($lines as $line) {
-                if (!str_contains($line, '@param')) {
-                    continue;
-                }
-
-                $parts = preg_split('#\s+#', $line, 4);
-                if (count($parts) < 3 || $parts[0] !== '@param') {
-                    continue;
-                }
-                $name = substr($parts[2], 1);
-                $type = $parts[1];
-
-                if (isset($defaultValues[$name])) {
-                    if ($type === 'bool' || $type === 'boolean') {
-                        $defaultValues[$name] = $defaultValues[$name] ? 'YES' : 'NO';
-                    } elseif ($type === 'int' || $type === 'integer') {
-                        $defaultValues[$name] = (int)$defaultValues[$name];
-                    } elseif ($type === 'float' || $type === 'double') {
-                        $defaultValues[$name] = (float)$defaultValues[$name];
-                    } elseif ($type === 'string') {
-                        $defaultValues[$name] = json_stringify($defaultValues[$name]);
-                    } elseif ($type === 'array') {
-                        $defaultValues[$name] = json_stringify($defaultValues[$name]);
-                    }
-                }
-
-                $params[$name] = isset($parts[3]) ? trim($parts[3]) : '';
-            }
-
-            if ($params) {
-                $shortNames = [];
-                foreach ($params as $name => $description) {
-                    $short = $name[0];
-                    if (isset($shortNames[$short])) {
-                        $shortNames[$short] = false;
-                    } else {
-                        $shortNames[$short] = $name;
-                    }
-                }
-                $shortNames = array_flip(array_filter($shortNames));
-
-                $width = 1;
-                foreach ($params as $name => $description) {
-                    $width = max($width, strlen($name) + 2 + (isset($shortNames[$name]) ? 4 : 0));
-                }
-                $this->console->writeLn('  Options:');
-
-                foreach ($params as $name => $value) {
-                    $option = '--' . $name;
-                    if (isset($shortNames[$name])) {
-                        $option .= ', -' . $shortNames[$name];
-                    }
-
-                    $ov = $this->console->colorize($option, Console::FC_CYAN, $width);
-                    $vv = $value ? "  $value" : '';
-                    $dv = isset($defaultValues[$name]) ? " (default: $defaultValues[$name])" : '';
-                    $this->console->writeLn(['    %s %s %s', $ov, $vv, $dv]);
-                }
+            $helpMethod = basename($method, 'Action') . 'Help';
+            if (method_exists($instance, $helpMethod)) {
+                $instance->$helpMethod();
+            } else {
+                $this->_commandHelp($rm, $method);
             }
         }
     }
