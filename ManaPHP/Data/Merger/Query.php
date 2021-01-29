38001584229fc9cea61b38bfc14e8552c910bb2f
ManaPHP Mark <manaphp@qq.com>
2021-01-29 20:47:50 +0800

    Modified   ManaPHP/Data/Merger/Query.php

--- ManaPHP/Data/Merger/Query.php
+++ ManaPHP/Data/Merger/Query.php
@@ -44,7 +44,7 @@ class Query extends \ManaPHP\Data\Query
     {
         foreach ($_queries as $id => $query) {
             if (is_string($query)) {
-                $query = Di::getDefault()->getNew($query);
+                $query = $this->getNew($query);
             }
 
             if ($query instanceof QueryInterface) {
