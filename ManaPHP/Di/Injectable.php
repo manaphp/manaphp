efbb766a92fcfe842b143441b8c15586ead55915
ManaPHP Mark <manaphp@qq.com>
2021-02-20 21:10:21 +0800

    Modified   ManaPHP/Di/Injectable.php

--- ManaPHP/Di/Injectable.php
+++ ManaPHP/Di/Injectable.php
@@ -5,37 +5,9 @@ namespace ManaPHP\Di;
 interface Injectable
 {
     /**
-     * @param string $class
-     * @param array  $params
-     *
-     * @return mixed
-     */
-    public function getNew($class, $params = []);
-
-    /**
-     * @param string $name
-     *
-     * @return mixed
-     */
-    public function getShared($name);
-
-    /**
      * @param \ManaPHP\Di\ContainerInterface $container
      *
      * @return static
      */
     public function setContainer($container);
-
-    /**
-     * @return \ManaPHP\Di\ContainerInterface
-     */
-    public function getContainer();
-
-    /**
-     * @param string $name
-     * @param mixed  $target
-     *
-     * @return static
-     */
-    public function inject($name, $target);
 }
