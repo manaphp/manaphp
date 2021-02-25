c7cdb4ce18f702a08fa5f823960de307bcdc9f17
ManaPHP Mark <manaphp@qq.com>
2021-02-25 20:19:32 +0800

    Modified   ManaPHP/Logging/Logger/Adapter/Db/manaphp_log.sql

--- ManaPHP/Logging/Logger/Adapter/Db/manaphp_log.sql
+++ ManaPHP/Logging/Logger/Adapter/Db/manaphp_log.sql
@@ -1,17 +1,17 @@
 CREATE TABLE `manaphp_log`
 (
-    `log_id`       int(11)                         NOT NULL AUTO_INCREMENT,
+    `log_id`       int(11) NOT NULL AUTO_INCREMENT,
     `hostname`     varchar(16)                     NOT NULL,
     `client_ip`    varchar(15) CHARACTER SET ascii NOT NULL,
     `request_id`   varchar(64) CHARACTER SET ascii NOT NULL,
     `category`     varchar(64) CHARACTER SET ascii NOT NULL,
     `level`        char(8) CHARACTER SET ascii     NOT NULL,
     `file`         varchar(64)                     NOT NULL,
-    `line`         int(11)                         NOT NULL,
+    `line`         int(11) NOT NULL,
     `message`      varchar(4000)                   NOT NULL,
     `timestamp`    float                           NOT NULL,
-    `created_time` int(11)                         NOT NULL,
+    `created_time` int(11) NOT NULL,
     PRIMARY KEY (`log_id`),
-    KEY `created_time` (`created_time`)
+    KEY            `created_time` (`created_time`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8
