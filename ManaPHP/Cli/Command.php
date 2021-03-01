68b271ede677df35fff6ffedbdfc646ea2182f54
ManaPHP Mark <manaphp@qq.com>
2021-03-01 19:50:52 +0800

    Modified   ManaPHP/Cli/Command.php

--- ManaPHP/Cli/Command.php
+++ ManaPHP/Cli/Command.php
@@ -2,26 +2,10 @@
 
 namespace ManaPHP\Cli;
 
-use ManaPHP\Http\Dispatcher\NotFoundActionException;
-
 /**
  * @property-read \ManaPHP\Cli\ConsoleInterface $console
  */
 abstract class Command extends \ManaPHP\Controller
 {
-    /**
-     * @param string $action
-     *
-     * @return void
-     * @throws NotFoundActionException
-     *
-     */
-    public function validateInvokable($action)
-    {
-        $method = $action . 'Action';
 
-        if (!in_array($method, get_class_methods($this), true)) {
-            throw new NotFoundActionException(['`%s::%s` method does not exist', static::class, $method]);
-        }
-    }
 }
