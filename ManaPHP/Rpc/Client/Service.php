da3b478de00367e8609586dfceebcd9a1f58042b
ManaPHP Mark <manaphp@qq.com>
2021-03-18 21:25:34 +0800

    Modified   ManaPHP/Rpc/Client/Service.php

--- ManaPHP/Rpc/Client/Service.php
+++ ManaPHP/Rpc/Client/Service.php
@@ -16,11 +16,6 @@ class Service implements Injectable
     protected $self;
 
     /**
-     * @var string
-     */
-    protected $endpoint;
-
-    /**
      * @var \ManaPHP\Rpc\ClientInterface
      */
     protected $rpcClient;
@@ -42,23 +37,11 @@ class Service implements Injectable
     {
         if (is_string($options)) {
             $options = ['endpoint' => $options];
-        }
-
-        if ($endpoint = $options['endpoint'] ?? null) {
-            $path = rtrim(parse_url($endpoint, PHP_URL_PATH), '/');
-            if ($path === '' || $path === '/api') {
-                $class_name = static::class;
-                $service = basename(substr($class_name, strrpos($class_name, '\\') + 1), 'Service');
-
-                $options['endpoint'] = rtrim($endpoint, '/') . '/' . Str::snakelize($service);
-            }
-        }
-
-        if (!$endpoint = $this->endpoint) {
+        } elseif (!isset($options['endpoint'])) {
             throw new MisuseException('missing endpoint config');
         }
 
-        $scheme = parse_url($endpoint, PHP_URL_SCHEME);
+        $scheme = parse_url($options['endpoint'], PHP_URL_SCHEME);
         if ($scheme === 'ws' || $scheme === 'wss') {
             $class = 'ManaPHP\\Rpc\\Client\\Adapter\\Ws';
         } elseif ($scheme === 'http' || $scheme === 'https') {
