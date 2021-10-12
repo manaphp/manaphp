96f1c9ed22a39a8ad9af2a8d06d173d7fe681420
ManaPHP Mark <manaphp@qq.com>
2021-10-12 20:42:50 +0800

    move cors to middlewares

--- app-api/config/app.php
+++ app-api/config/app.php
@@ -1,27 +1,27 @@
 <?php
 
 return [
-    'id'         => 'api',
-    'env'        => env('APP_ENV', 'prod'),
-    'debug'      => env('APP_DEBUG', false),
-    'version'    => '1.1.1',
-    'timezone'   => 'PRC',
-    'master_key' => env('MASTER_KEY'),
-    'params'     => [],
-    'aliases'    => [],
-    'components' => [
+    'id'          => 'api',
+    'env'         => env('APP_ENV', 'prod'),
+    'debug'       => env('APP_DEBUG', false),
+    'version'     => '1.1.1',
+    'timezone'    => 'PRC',
+    'master_key'  => env('MASTER_KEY'),
+    'params'      => [],
+    'aliases'     => [],
+    'components'  => [
         'httpServer' => ['port' => 9501, 'worker_num' => 4, 'max_request' => 1000000],
         'db'         => [env('DB_URL')],
         'redis'      => [env('REDIS_URL')],
         'logger'     => ['level' => env('LOGGER_LEVEL', 'info')],
     ],
-    'services'   => [],
-    'listeners'  => [],
-    'plugins'    => [
+    'services'    => [],
+    'listeners'   => [],
+    'plugins'     => [
         'debugger',
-        'cors',
         //'slowlog',
         //'logger',
     ],
-    'tracers'    => ['*'],
+    'tracers'     => ['*'],
+    'middlewares' => ['cors']
 ];
