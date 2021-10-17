02df81b3145d88a9e4a8b31905e9f0df2420fcc0
ManaPHP Mark <manaphp@qq.com>
2021-10-17 15:36:10 +0800

    using class_alias to implement Mvc\View\Flash

--- ManaPHP/Mvc/Provider.php
+++ ManaPHP/Mvc/Provider.php
@@ -9,7 +9,7 @@ class Provider extends \ManaPHP\Di\Provider
             'httpHandler'  => 'ManaPHP\Mvc\Handler',
             'errorHandler' => 'ManaPHP\Mvc\ErrorHandler',
             'view'         => 'ManaPHP\Mvc\View',
-            'flash'        => 'ManaPHP\Mvc\View\Flash\Adapter\Direct',
+            'flash'        => 'ManaPHP\Mvc\View\Flash',
             'flashSession' => 'ManaPHP\Mvc\View\Flash\Adapter\Session',
             'viewsCache'   => ['ManaPHP\Caching\Cache\Adapter\Redis', 'prefix' => 'cache:views:'],
             'identity'     => 'ManaPHP\Identifying\Identity\Adapter\Session',
---
+++ ManaPHP/Mvc/View/Flash.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ManaPHP\Mvc\View;
+
+(static function () {
+    $class = 'ManaPHP\Mvc\View\Flash\Adapter\Direct';
+
+    class_alias($class, 'ManaPHP\Mvc\View\Flash');
+})();
