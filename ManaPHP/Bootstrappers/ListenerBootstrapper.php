581202daf719a24cff67cc1c7e1788448a2f6a34
ManaPHP Mark <manaphp@qq.com>
2021-10-30 10:08:35 +0800

    refactor Listeners

--- ManaPHP/Bootstrappers/ListenerBootstrapper.php
+++ ManaPHP/Bootstrappers/ListenerBootstrapper.php
@@ -10,10 +10,19 @@ class ListenerBootstrapper extends Component implements BootstrapperInterface
 {
     public function bootstrap()
     {
+        /** @var \ManaPHP\Event\ListenInterface $listener */
+
         foreach (LocalFS::glob('@app/Listeners/?*Listener.php') as $file) {
-            /** @var \ManaPHP\Event\ListenInterface $listener */
             $listener = $this->container->get('App\Listeners\\' . basename($file, '.php'));
             $listener->listen();
+        }
+
+        foreach (LocalFS::glob('@app/Areas/*', GLOB_ONLYDIR) as $item) {
+            $area = basename($item);
+            foreach (LocalFS::glob("$item/Listeners/?*Listener.php") as $file) {
+                $listener = $this->container->get("App\\Areas\\$area\\Listeners\\" . basename($file, '.php'));
+                $listener->listen();
+            }
         }
     }
 }
