339921d49221ee19bcf783b198d821e12e2060c4
ManaPHP Mark <manaphp@qq.com>
2021-10-30 10:14:54 +0800

    refactor Middlewares

--- ManaPHP/Bootstrappers/MiddlewareBootstrapper.php
+++ ManaPHP/Bootstrappers/MiddlewareBootstrapper.php
@@ -6,30 +6,46 @@ use ManaPHP\BootstrapperInterface;
 use ManaPHP\Component;
 use ManaPHP\Helper\LocalFS;
 
-/**
- * @property-read \ManaPHP\ConfigInterface $config
- */
 class MiddlewareBootstrapper extends Component implements BootstrapperInterface
 {
+    /**
+     * @var array
+     */
+    protected $middlewares = [];
+
+    /**
+     * @param array $options
+     */
+    public function __construct($options = [])
+    {
+        if (isset($options['middlewares'])) {
+            $this->middlewares = $options['middlewares'];
+        }
+    }
+
     public function bootstrap()
     {
+        /** @var \ManaPHP\Http\Middleware $middleware */
+
+        foreach ($this->middlewares as $definition) {
+            $plain = ucfirst($definition) . 'Middleware';
+            $class = "ManaPHP\Http\Middlewares\\$plain";
+
+            $middleware = $this->container->get($class);
+            $middleware->listen();
+        }
+
         foreach (LocalFS::glob('@app/Middlewares/?*Middleware.php') as $file) {
-            /** @var \ManaPHP\Http\Middleware $middleware */
             $middleware = $this->container->get('App\Middlewares\\' . basename($file, ".php"));
             $middleware->listen();
         }
 
-        $middlewares = $this->config->get('middlewares', []);
-        foreach ($middlewares as $definition) {
-            if (str_contains($definition, '\\')) {
-                $class = $definition;
-            } else {
-                $plain = ucfirst($definition) . 'Middleware';
-                $class = "ManaPHP\Http\Middlewares\\$plain";
+        foreach (LocalFS::glob('@app/Areas/*', GLOB_ONLYDIR) as $item) {
+            $area = basename($item);
+            foreach (LocalFS::glob("$item/Middlewares/?*Middleware.php") as $file) {
+                $middleware = $this->container->get("App\\Areas\\$area\\Middlewares\\" . basename($file, '.php'));
+                $middleware->listen();
             }
-
-            $middleware = $this->container->get($class);
-            $middleware->listen();
         }
     }
 }
