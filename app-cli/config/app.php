9bfd24682108671290f307f64c8b23f8d9f29cd1
ManaPHP Mark <manaphp@qq.com>
2021-11-01 20:59:40 +0800

    app.php

--- app-cli/config/app.php
+++ app-cli/config/app.php
@@ -1,24 +1,18 @@
 <?php
 
 return [
-    'id'         => 'cli',
-    'env'        => env('APP_ENV', 'prod'),
-    'debug'      => env('APP_DEBUG', false),
-    'version'    => '1.1.1',
-    'timezone'   => 'PRC',
-    'master_key' => env('MASTER_KEY'),
-    'params'     => [],
-    'aliases'    => [
+    'id'            => 'cli',
+    'env'           => env('APP_ENV', 'prod'),
+    'debug'         => env('APP_DEBUG', false),
+    'version'       => '1.1.1',
+    'timezone'      => 'PRC',
+    'aliases'       => [
     ],
-    'dependencies' => [
-  //      'db'         => env('DB_URL'),
-  //      'redis'      => env('REDIS_URL'),
-  //      'logger'     => ['level' => env('LOGGER_LEVEL', 'info')],
-  //      'amqpClient' => 'amqp://192.168.0.8/'
+    'dependencies'  => [
+        //      'db'         => env('DB_URL'),
+        //      'redis'      => env('REDIS_URL'),
+        //      'logger'     => ['level' => env('LOGGER_LEVEL', 'info')],
+        //      'amqpClient' => 'amqp://192.168.0.8/'
     ],
-    'services'   => [
-        'timeService' => ['endpoint' => 'http://localhost:85/time']
-    ],
-    'plugins'    => [],
-    'tracers'    => ['*'],
+    'bootstrappers' => []
 ];
