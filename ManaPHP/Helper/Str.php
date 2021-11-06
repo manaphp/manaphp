8dc9acb73e1d056b8c5ba92fb80e41b81bb21651
ManaPHP Mark <manaphp@qq.com>
2021-11-06 18:06:36 +0800

    remove some St::xx method

--- ManaPHP/Helper/Str.php
+++ ManaPHP/Helper/Str.php
@@ -8,54 +8,6 @@ use ManaPHP\Exception\NotSupportedException;
 class Str
 {
     /**
-     * @param string $haystack
-     * @param string $needle
-     * @param bool   $ignoreCase
-     *
-     * @return bool
-     */
-    public static function contains($haystack, $needle, $ignoreCase = false)
-    {
-        if ($ignoreCase) {
-            return stripos($haystack, $needle) !== false;
-        } else {
-            return str_contains($haystack, $needle);
-        }
-    }
-
-    /**
-     * @param string $haystack
-     * @param string $needle
-     * @param bool   $ignoreCase
-     *
-     * @return bool
-     */
-    public static function startsWith($haystack, $needle, $ignoreCase = false)
-    {
-        if ($ignoreCase) {
-            return stripos($haystack, $needle) === 0;
-        } else {
-            return str_starts_with($haystack, $needle);
-        }
-    }
-
-    /**
-     * @param string $haystack
-     * @param string $needle
-     * @param bool   $ignoreCase
-     *
-     * @return bool
-     */
-    public static function endsWith($haystack, $needle, $ignoreCase = false)
-    {
-        if ($ignoreCase) {
-            return strripos($haystack, $needle) === strlen($haystack) - strlen($needle);
-        } else {
-            return str_ends_with($haystack, $needle);
-        }
-    }
-
-    /**
      * snake_case
      *
      * @param string $str
--- tests/HelpStrTest.php
+++ tests/HelpStrTest.php
@@ -7,36 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class HelpStrTest extends TestCase
 {
-    public function test_contains()
-    {
-        $this->assertTrue(Str::contains('manaphp', 'mana'));
-        $this->assertTrue(Str::contains('manaphp', 'manaphp'));
-        $this->assertFalse(Str::contains('manaphp', 'ManaPHP'));
-
-        $this->assertTrue(Str::contains('manaphp', 'Mana', true));
-        $this->assertTrue(Str::contains('manaphp', 'Manaphp', true));
-    }
-
-    public function test_startsWith()
-    {
-        $this->assertTrue(Str::startsWith('manaphp', 'mana'));
-        $this->assertTrue(Str::startsWith('manaphp', 'manaphp'));
-        $this->assertFalse(Str::startsWith('manaphp', 'Mana'));
-
-        $this->assertTrue(Str::startsWith('manaphp', 'Mana', true));
-        $this->assertTrue(Str::startsWith('manaphp', 'Manaphp', true));
-    }
-
-    public function test_endsWith()
-    {
-        $this->assertTrue(Str::endsWith('manaphp', 'php'));
-        $this->assertTrue(Str::endsWith('manaphp', 'manaphp'));
-        $this->assertFalse(Str::endsWith('manaphp', 'mana'));
-
-        $this->assertTrue(Str::endsWith('manaphp', 'PHP', true));
-        $this->assertTrue(Str::endsWith('manaphp', 'Manaphp', true));
-    }
-
     public function test_underscore()
     {
         $this->assertEquals('raw_scaled_scorer', Str::snakelize('RawScaledScorer'));
