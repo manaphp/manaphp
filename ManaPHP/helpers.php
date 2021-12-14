bf5bf804f824e234c93321d08744afcfe025f31f
ManaPHP Mark <manaphp@qq.com>
2021-12-14 20:56:44 +0800

    add strict-types for helpers

--- ManaPHP/helpers.php
+++ ManaPHP/helpers.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use ManaPHP\Di\ContainerInterface;
 use ManaPHP\Exception\AbortException;
@@ -8,12 +9,7 @@ use ManaPHP\ConfigInterface;
 use ManaPHP\AliasInterface;
 
 if (!function_exists('json_parse')) {
-    /**
-     * @param string $str
-     *
-     * @return mixed
-     */
-    function json_parse($str)
+    function json_parse(string $str): mixed
     {
         /** @noinspection NotOptimalIfConditionsInspection */
         if (($json = json_decode($str, true, 16, JSON_THROW_ON_ERROR)) === null && $str !== 'null') {
@@ -25,13 +21,7 @@ if (!function_exists('json_parse')) {
 }
 
 if (!function_exists('json_stringify')) {
-    /**
-     * @param mixed $json
-     * @param int   $options
-     *
-     * @return string
-     */
-    function json_stringify($json, $options = 0)
+    function json_stringify(mixed $json, int $options = 0): string
     {
         $options |= JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_SUBSTITUTE;
 
@@ -44,12 +34,7 @@ if (!function_exists('json_stringify')) {
 }
 
 if (!function_exists('xml_decode')) {
-    /**
-     * @param string $xml
-     *
-     * @return array|null
-     */
-    function xml_decode($xml)
+    function xml_decode(string $xml): ?array
     {
         if (($ret = @simplexml_load_string($xml, null, LIBXML_NOCDATA | LIBXML_NOBLANKS)) === false) {
             return null;
@@ -68,12 +53,7 @@ if (!function_exists('xml_decode')) {
 }
 
 if (!function_exists('container')) {
-    /**
-     * @param string $name
-     *
-     * @return mixed
-     */
-    function container($name = null)
+    function container(?string $name = null): mixed
     {
         /** @var ContainerInterface $container */
         $container = $GLOBALS['ManaPHP\Di\ContainerInterface'];
@@ -82,91 +62,49 @@ if (!function_exists('container')) {
 }
 
 if (!function_exists('env')) {
-    /**
-     * @param string $key
-     * @param mixed  $default
-     *
-     * @return mixed
-     */
-    function env($key = null, $default = null)
+    function env(?string $key = null, mixed $default = null): mixed
     {
         return container(\ManaPHP\EnvInterface::class)->get($key, $default);
     }
 }
 
 if (!function_exists('config_get')) {
-    /**
-     * @param string $name
-     * @param mixed  $default
-     *
-     * @return mixed
-     */
-    function config_get($name, $default = null)
+    function config_get(string $name, mixed $default = null): mixed
     {
         return container(ConfigInterface::class)->get($name, $default);
     }
 }
 
 if (!function_exists('dd')) {
-    /**
-     * @param mixed $message
-     *
-     * @return void
-     */
-    function dd($message)
+    function dd(mixed $message): void
     {
         container(\ManaPHP\Debugging\DataDumpInterface::class)->output($message);
     }
 }
 
 if (!function_exists('path')) {
-    /**
-     * @param string $path
-     *
-     * @return string
-     */
-    function path($path)
+    function path(string $path): string
     {
-        return $path ? container(AliasInterface::class)->resolve($path) : container(AliasInterface::class)->get();
+        return container(AliasInterface::class)->resolve($path);
     }
 }
 
 if (!function_exists('jwt_encode')) {
-    /**
-     * @param array  $claims
-     * @param int    $ttl
-     * @param string $scope
-     *
-     * @return string
-     */
-    function jwt_encode($claims, $ttl, $scope)
+    function jwt_encode(array $claims, int $ttl, string $scope): string
     {
         return container(\ManaPHP\Token\ScopedJwtInterface::class)->encode($claims, $ttl, $scope);
     }
 }
 
 if (!function_exists('jwt_decode')) {
-    /**
-     * @param string $token
-     * @param string $scope
-     * @param bool   $verify
-     *
-     * @return array
-     */
-    function jwt_decode($token, $scope, $verify = true)
+    function jwt_decode(string $token, string $scope, bool $verify = true): array
     {
         return container(\ManaPHP\Token\ScopedJwtInterface::class)->decode($token, $scope, $verify);
     }
 }
 
 if (!function_exists('jwt_verify')) {
-    /**
-     * @param string $token
-     * @param string $scope
-     *
-     * @return void
-     */
-    function jwt_verify($token, $scope)
+    function jwt_verify(string $token, string $scope): void
     {
         container(\ManaPHP\Token\ScopedJwt::class)->verify($token, $scope);
     }
@@ -174,12 +112,12 @@ if (!function_exists('jwt_verify')) {
 
 if (!function_exists('input')) {
     /**
-     * @param string $name
-     * @param mixed  $defaultOrRules =\PHPSTORM_META\validator_rule()
+     * @param ?string $name
+     * @param mixed   $defaultOrRules =\PHPSTORM_META\validator_rule()
      *
      * @return mixed
      */
-    function input($name = null, $defaultOrRules = null)
+    function input(?string $name = null, mixed $defaultOrRules = null): mixed
     {
         static $request;
         if (!$request) {
@@ -189,7 +127,9 @@ if (!function_exists('input')) {
         if ($defaultOrRules && is_array($defaultOrRules)) {
             $value = $request->get($name, $defaultOrRules['default'] ?? null);
 
-            return container(\ManaPHP\Validating\ValidatorInterface::class)->validateValue($name, $value, $defaultOrRules);
+            return container(\ManaPHP\Validating\ValidatorInterface::class)->validateValue(
+                $name, $value, $defaultOrRules
+            );
         } else {
             return $request->get($name, $defaultOrRules);
         }
@@ -197,23 +137,14 @@ if (!function_exists('input')) {
 }
 
 if (!function_exists('abort')) {
-    /**
-     * @return void
-     * @throws \ManaPHP\Exception\AbortException
-     */
-    function abort()
+    function abort(): void
     {
         throw new AbortException();
     }
 }
 
 if (!function_exists('seconds')) {
-    /**
-     * @param string $str
-     *
-     * @return int
-     */
-    function seconds($str)
+    function seconds(string $str): int
     {
         if (preg_match('#^([\d.]+)([smhdMy]?)$#', $str, $match)) {
             $units = ['' => 1, 's' => 1, 'm' => 60, 'h' => 3600, 'd' => 86400, 'M' => 2592000, 'y' => 31536000];
@@ -227,49 +158,28 @@ if (!function_exists('seconds')) {
 }
 
 if (!function_exists('e')) {
-    /**
-     * @param string $value
-     * @param bool   $doubleEncode
-     *
-     * @return string
-     */
-    function e($value, $doubleEncode = true)
+    function e(string $value, bool $doubleEncode = true): string
     {
         return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', $doubleEncode);
     }
 }
 
 if (!function_exists('t')) {
-    /**
-     * @param string $id
-     * @param array  $bind
-     *
-     * @return string
-     */
-    function t($id, $bind = [])
+    function t(string $id, array $bind = []): string
     {
         return container(\ManaPHP\I18n\TranslatorInterface::class)->translate($id, $bind);
     }
 }
 
 if (!function_exists('base_url')) {
-    /**
-     * @return string
-     */
-    function base_url()
+    function base_url(): string
     {
         return container(\ManaPHP\AliasInterface::class)->get('@web');
     }
 }
 
 if (!function_exists('console_log')) {
-    /**
-     * @param string $level
-     * @param mixed  $message
-     *
-     * @return void
-     */
-    function console_log($level, $message)
+    function console_log(string $level, mixed $message): void
     {
         if (is_array($message)) {
             $message = sprintf(...$message);
@@ -279,14 +189,7 @@ if (!function_exists('console_log')) {
 }
 
 if (!function_exists('apcu_remember')) {
-    /**
-     * @param string   $key
-     * @param int      $ttl
-     * @param callable $callback
-     *
-     * @return mixed
-     */
-    function apcu_remember($key, $ttl, $callback)
+    function apcu_remember(string $key, int $ttl, callable $callback): mixed
     {
         $value = apcu_fetch($key, $success);
         if (!$success) {
