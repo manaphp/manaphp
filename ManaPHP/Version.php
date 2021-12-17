39cd4d649e60c1e79e6b5210b17be4f81aa18318
ManaPHP Mark <manaphp@qq.com>
2021-12-17 20:23:44 +0800

    add strict-types for Version

--- ManaPHP/Version.php
+++ ManaPHP/Version.php
@@ -1,13 +1,11 @@
 <?php
+declare(strict_types=1);
 
 namespace ManaPHP;
 
 class Version
 {
-    /**
-     * @return string
-     */
-    public static function get()
+    public static function get(): string
     {
         return '2.0.6';
     }
