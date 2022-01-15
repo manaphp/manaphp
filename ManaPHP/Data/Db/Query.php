7b0ec5e58af8a110c6c50489f16c961564ab46c7
ManaPHP Mark <manaphp@qq.com>
2022-01-15 12:57:59 +0800

    fix Condition is always 'false' because 'int $divisor' is evaluated at this point

--- ManaPHP/Data/Db/Query.php
+++ ManaPHP/Data/Db/Query.php
@@ -133,14 +133,6 @@ class Query extends AbstractQuery
 
     public function whereMod(string $field, int $divisor, int $remainder): static
     {
-        if (!is_int($divisor)) {
-            throw new MisuseException('divisor must be an integer');
-        }
-
-        if (!is_int($remainder)) {
-            throw new MisuseException('remainder must be an integer');
-        }
-
         $this->conditions[] = "$field%$divisor=$remainder";
 
         return $this;
