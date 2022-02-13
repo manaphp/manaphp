90d2982074f47e86bb4426467c4e1fdb80bec2d3
ManaPHP Mark <manaphp@qq.com>
2022-02-13 11:03:26 +0800

    refactor ManaPHP\Cli\Command

--- framework/Cli/Command.php
+++ framework/Cli/Command.php
@@ -3,10 +3,16 @@ declare(strict_types=1);
 
 namespace ManaPHP\Cli;
 
+use ManaPHP\Component;
+use ManaPHP\Logging\Logger\LogCategorizable;
+
 /**
  * @property-read \ManaPHP\Cli\ConsoleInterface $console
  */
-class Command extends \ManaPHP\Controller
+class Command extends Component implements LogCategorizable
 {
-
+    public function categorizeLog(): string
+    {
+        return basename(str_replace('\\', '.', static::class), 'Command');
+    }
 }
