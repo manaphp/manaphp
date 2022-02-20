4051372b56f2e167403a2a9bc0db974b4df211fe
ManaPHP Mark <manaphp@qq.com>
2022-02-20 22:01:11 +0800

    add Authorize attribute

--- app-admin/app/Controllers/IndexController.php
+++ app-admin/app/Controllers/IndexController.php
@@ -3,13 +3,11 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use ManaPHP\Http\Controller\Attribute\Authorize;
+
+#[Authorize('user')]
 class IndexController extends Controller
 {
-    public function getAcl(): array
-    {
-        return ['*' => 'user'];
-    }
-
     public function indexAction()
     {
 
