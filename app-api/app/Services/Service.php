3d57d5cb2c09086447b2c2c8f5f64ed0a84791ec
ManaPHP Mark <manaphp@qq.com>
2022-03-20 12:03:37 +0800

    fix app-api App\Services\Service

--- app-api/app/Services/Service.php
+++ app-api/app/Services/Service.php
@@ -7,7 +7,7 @@ namespace App\Services;
  * @property-read \ManaPHP\Logging\LoggerInterface       $logger
  * @property-read \ManaPHP\Identifying\IdentityInterface $identity
  * @property-read \ManaPHP\Data\RedisDbInterface         $redisDb
- * @property-read \ManaPHP\Data\RedisCacheInterface      $redisCache                                 $redisCache
+ * @property-read \ManaPHP\Data\RedisCacheInterface      $redisCache
  * @property-read \ManaPHP\Data\RedisBrokerInterface     $redisBroker
  */
 class Service extends \ManaPHP\Service
