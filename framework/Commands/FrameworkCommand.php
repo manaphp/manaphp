d9a18d915d1b5bb73383e856aef0f4ba23c057fb
ManaPHP Mark <manaphp@qq.com>
2022-04-08 20:18:26 +0800

    fix ManaPHP\Commands\FrameworkCommand

--- framework/Commands/FrameworkCommand.php
+++ framework/Commands/FrameworkCommand.php
@@ -106,8 +106,8 @@ class FrameworkCommand extends Command
         $this->console->writeLn('------------------------------------------------------');
 
         foreach ($fileLines as $file => $line) {
-            $cut_file = substr($file, strpos($file, 'ManaPHP'));
-            $this->console->writeLn(sprintf('%3d %3d %.3f %s', $i++, $line, $line / $totalLines * 100, $cut_file));
+            $cut_file = substr($file, strpos($file, 'framework'));
+            $this->console->writeLn(sprintf('%3d %3d %.2f%% %s', $i++, $line, $line / $totalLines * 100, $cut_file));
         }
 
         $this->console->writeLn('------------------------------------------------------');
