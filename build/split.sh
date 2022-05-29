240cf79dc08862aedbc737eee5a0d1087fb2cd99
ManaPHP Mark <manaphp@qq.com>
2022-05-29 11:29:13 +0800

    build/split.sh

--- build/split.sh
+++ build/split.sh
@@ -1,3 +1,10 @@
-./git-subsplit.sh init git@github.com:manaphp/manaphp.git
-./git-subsplit.sh publish framework:git@github.com:manaphp/framework.git
-rm -rf .subsplit/
+#!/usr/bin/env bash
+
+BUILDER_DIR=$(realpath $(dirname "$0"))
+
+(cd $BUILDER_DIR\
+  &&rm -rf .subsplit/\
+  &&./git-subsplit.sh init git@github.com:manaphp/manaphp.git\
+  &&./git-subsplit.sh publish framework:git@github.com:manaphp/framework.git\
+  &&rm -rf .subsplit/
+)
