07f71f48ac3ef3fc42ab6f538d85b2ae6afe8dd1
ManaPHP Mark <manaphp@qq.com>
2022-06-03 08:47:49 +0800

    app-admin/app/Controllers/TestController

--- app-admin/app/Controllers/TestController.php
+++ app-admin/app/Controllers/TestController.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use ManaPHP\Http\Controller\Attribute\Authorize;
+
+#[Authorize('admin')]
 class TestController extends Controller
 {
     public function indexAction()
