fffba206f6b3b92152d777adf16bc469bbd81c07
ManaPHP Mark <manaphp@qq.com>
2023-09-23 11:39:26 +0800

    bootstrap.php

--- app-api/manacli.php
+++ app-api/manacli.php
@@ -1,14 +1,6 @@
 #!/usr/bin/env php
 <?php
 
-use ManaPHP\Di\Container;
-use ManaPHP\Kernel;
+require __DIR__ . '/bootstrap.php';
 
-ini_set('memory_limit', -1);
-ini_set('html_errors', 'off');
-ini_set('default_socket_timeout', -1);
-
-require __DIR__ . '/vendor/autoload.php';
-
-$container = new Container([Kernel::class => ['rootDir' => __DIR__]]);
-$container->get(Kernel::class)->start('ManaPHP\Cli\ServerInterface');
+bootstrap('ManaPHP\Cli\ServerInterface');
