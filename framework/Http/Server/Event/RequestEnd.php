df8be5f867a3c58b4c4b0e32a54b8938769b5f8f
ManaPHP Mark <manaphp@qq.com>
2023-10-15 14:48:59 +0800

    \ManaPHP\Http\Server\Event\RequestEnd

--- framework/Http/Server/Event/RequestEnd.php
+++ framework/Http/Server/Event/RequestEnd.php
@@ -3,17 +3,28 @@ declare(strict_types=1);
 
 namespace ManaPHP\Http\Server\Event;
 
+use JsonSerializable;
 use ManaPHP\Eventing\Attribute\Verbosity;
 use ManaPHP\Http\RequestInterface;
 use ManaPHP\Http\ResponseInterface;
 
 #[Verbosity(Verbosity::LOW)]
-class RequestEnd
+class RequestEnd implements JsonSerializable
 {
     public function __construct(
         public RequestInterface $request,
         public ResponseInterface $response,
     ) {
 
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'uri'            => $this->request->getUri(),
+            'http_code'      => $this->response->getStatusCode(),
+            'content-type'   => $this->response->getContentType(),
+            'content-length' => strlen($this->response->getContent()),
+        ];
     }
 }
