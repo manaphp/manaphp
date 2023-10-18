72ec7bcfe51319b098646445daf0f48128fed3e3
ManaPHP Mark <manaphp@qq.com>
2023-10-18 19:59:57 +0800

    kernel.php

--- app-admin/config/kernel.php
+++ app-admin/config/kernel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 return [
     'ManaPHP\KernelInterface' => [
         'bootstrappers' => [
+            ManaPHP\Eventing\ListenerBootstrapper::class,
             ManaPHP\Debugging\DebuggerBootstrapper::class,
             ManaPHP\Http\FilterBootstrapper::class,
             ManaPHP\Eventing\TracerBootstrapper::class,
