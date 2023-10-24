0d3eb96f8960d38e345225a3c7bb63dafd65d91e
ManaPHP Mark <manaphp@qq.com>
2023-10-24 22:19:27 +0800

    fix Useless trailing comma

--- framework/Http/Metrics/FormatterInterface.php
+++ framework/Http/Metrics/FormatterInterface.php
@@ -8,9 +8,9 @@ interface FormatterInterface
     public const GAUGE = 1;
     public const COUNTER = 2;
 
-    public function gauge(string $name, int|array $value, array $labels = [], array $levels = [],): string;
+    public function gauge(string $name, int|array $value, array $labels = [], array $levels = []): string;
 
-    public function counter(string $name, int|array $value, array $labels = [], array $levels = [],): string;
+    public function counter(string $name, int|array $value, array $labels = [], array $levels = []): string;
 
     public function number(string $name, int|array $value, array $labels = [], array $levels = []): string;
 
