cc3e9340114ed4816672c05b92e0d5b241ace4d5
ManaPHP Mark <manaphp@qq.com>
2023-11-23 19:31:37 +0800

    ManaPHP\Pooling\Pool\Event

--- framework/Pooling/Pool/Event/PoolPopping.php
+++ framework/Pooling/Pool/Event/PoolPopping.php
@@ -24,6 +24,5 @@ class PoolPopping
              'type'  => $this->type
             ]
         );
-
     }
 }
--- framework/Pooling/Pool/Event/PoolPush.php
+++ framework/Pooling/Pool/Event/PoolPush.php
@@ -4,9 +4,28 @@ declare(strict_types=1);
 namespace ManaPHP\Pooling\Pool\Event;
 
 use ManaPHP\Eventing\Attribute\Verbosity;
+use ManaPHP\Pooling\PoolsInterface;
+use Stringable;
 
 #[Verbosity(Verbosity::HIGH)]
-class PoolPush extends PoolBase
+class PoolPush implements Stringable
 {
+    public function __construct(
+        public PoolsInterface $pools,
+        public object $owner,
+        public object $instance,
+        public string $type,
+    ) {
 
+    }
+
+    public function __toString(): string
+    {
+        return json_stringify(
+            ['owner'    => $this->owner::class,
+             'instance' => $this->instance::class . '#' . spl_object_id($this->instance),
+             'type'     => $this->type
+            ]
+        );
+    }
 }
