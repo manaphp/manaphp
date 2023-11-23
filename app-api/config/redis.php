f6261f134d415f295a0d3141a632ba5e1c9118c9
ManaPHP Mark <manaphp@qq.com>
2023-11-23 20:03:15 +0800

    redis.php

---
+++ app-api/config/redis.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'ManaPHP\Redis\RedisInterface'       => new \ManaPHP\Di\Pool([
+        'default' => ['uri' => env('REDIS_URL')],
+        'db'      => '#default',
+        'cache'   => '#default',
+        'broker'  => '#default',
+    ]),
+    'ManaPHP\Redis\RedisDbInterface'     => 'ManaPHP\Redis\RedisInterface#db',
+    'ManaPHP\Redis\RedisBrokerInterface' => 'ManaPHP\Redis\RedisInterface#broker',
+    'ManaPHP\Redis\RedisCacheInterface'  => 'ManaPHP\Redis\RedisInterface#cache'
+];
