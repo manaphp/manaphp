6fbeadc895668021db7e448a6b2fd0c62104cfc6
ManaPHP Mark <manaphp@qq.com>
2023-11-25 09:03:52 +0800

    redis.php

--- app-api/config/dependencies.php
+++ app-api/config/dependencies.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 
 return [
-    'ManaPHP\Redis\RedisInterface'          => ['uri' => env('REDIS_URL')],
     'Psr\Log\LoggerInterface'               => ['class' => 'ManaPHP\Logging\Logger\Adapter\File',
                                                 'level' => env('LOGGER_LEVEL', 'info')],
     'ManaPHP\Identifying\IdentityInterface' => 'ManaPHP\Identifying\Identity\Adapter\Jwt',
@@ -10,5 +9,5 @@ return [
                                                 'prefix' => '/api',
     ],
     'ManaPHP\Security\CryptInterface'       => ['master_key' => 'dev'],
-    'ManaPHP\Eventing\TracerInterface'      => ['verbosity' => \ManaPHP\Eventing\Attribute\Verbosity::MEDIUM],
+    'ManaPHP\Eventing\TracerInterface'      => ['verbosity' => \ManaPHP\Eventing\Attribute\Verbosity::HIGH],
 ];
