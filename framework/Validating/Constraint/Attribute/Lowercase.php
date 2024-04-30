69eaad641652f92f62cb6e6e6c6d0d2ce1ea0b1b
ManaPHP Mark <manaphp@qq.com>
2024-04-30 20:11:38 +0800

    framework/Validating/Constraint/Attribute/Lowercase.php

--- framework/Validating/Constraint/Attribute/Lowercase.php
+++ framework/Validating/Constraint/Attribute/Lowercase.php
@@ -10,10 +10,18 @@ use ManaPHP\Validating\Validation;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Lowercase extends AbstractConstraint
 {
+    public function __construct(protected bool $sanitize = true, protected ?string $message = null)
+    {
+        parent::__construct($message);
+    }
+
     public function validate(Validation $validation): bool
     {
-        $validation->value = \strtolower($validation->value);
-
-        return true;
+        if ($this->sanitize) {
+            $validation->value = \strtolower($validation->value);
+            return true;
+        } else {
+            return $validation->value === \strtolower($validation->value);
+        }
     }
 }
