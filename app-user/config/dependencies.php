0507e9359d42ef55112243580ff4995619dc3cd9
ManaPHP Mark <manaphp@qq.com>
2024-05-02 22:07:41 +0800

    user

--- app-user/config/dependencies.php
+++ app-user/config/dependencies.php
@@ -6,7 +6,7 @@ return [
                                                 'level' => env('LOGGER_LEVEL', 'info')],
     'ManaPHP\Identifying\IdentityInterface' => 'ManaPHP\Identifying\Identity\Adapter\Jwt',
     'ManaPHP\Http\RouterInterface'          => ['class'  => 'App\Router',
-                                                'prefix' => '/user',
+                                                'prefix' => '',
     ],
     'ManaPHP\Security\CryptInterface'       => ['master_key' => 'dev'],
     'ManaPHP\Eventing\TracerInterface'      => ['verbosity' => \ManaPHP\Eventing\Attribute\Verbosity::HIGH],
