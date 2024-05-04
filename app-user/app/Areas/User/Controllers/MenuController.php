03645b6d80d428098f17d9b819fd9ab550227de7
ManaPHP Mark <manaphp@qq.com>
2024-05-04 12:01:49 +0800

    route separator with -

--- app-user/app/Areas/User/Controllers/MenuController.php
+++ app-user/app/Areas/User/Controllers/MenuController.php
@@ -13,8 +13,8 @@ class MenuController extends Controller
         $menu = [
             ['group_name' => '个人中心',
              'items'      => [
-                 ['item_name' => '最近登录', 'url' => '/user/login_log/latest'],
-                 ['item_name' => '最近操作', 'url' => '/user/action_log/latest'],
+                 ['item_name' => '最近登录', 'url' => '/user/login-log/latest'],
+                 ['item_name' => '最近操作', 'url' => '/user/action-log/latest'],
                  ['item_name' => '修改密码', 'url' => '/user/password/change'],
              ]],
         ];
