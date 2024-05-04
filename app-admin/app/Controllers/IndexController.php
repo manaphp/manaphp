c2812629deeb462729a2add361c4278000c0d7db
ManaPHP Mark <manaphp@qq.com>
2024-05-04 21:43:08 +0800

    \App\Controllers\IndexController::metricsAction

--- app-admin/app/Controllers/IndexController.php
+++ app-admin/app/Controllers/IndexController.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use ManaPHP\Di\Attribute\Autowired;
 use ManaPHP\Http\Controller\Attribute\Authorize;
+use ManaPHP\Http\Metrics\ExporterInterface;
+use ManaPHP\Http\Router\Attribute\GetMapping;
 use ManaPHP\Http\Router\Attribute\RequestMapping;
 use ManaPHP\Mvc\View\Attribute\ViewGetMapping;
 
@@ -11,9 +14,17 @@ use ManaPHP\Mvc\View\Attribute\ViewGetMapping;
 #[RequestMapping('')]
 class IndexController extends Controller
 {
+    #[Autowired] protected ExporterInterface $exporter;
+
     #[ViewGetMapping('/')]
     public function indexAction()
     {
 
+    }
+
+    #[GetMapping]
+    public function metricsAction()
+    {
+        return $this->exporter->export();
     }
 }
