7fd06a3acd9cefc2c353c94748f72c71a3a7de0b
ManaPHP Mark <manaphp@qq.com>
2024-05-12 12:03:06 +0800

    app-user

--- app-user/app/Areas/User/Controllers/ActionLogController.php
+++ app-user/app/Areas/User/Controllers/ActionLogController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 namespace App\Areas\User\Controllers;
 
 use App\Controllers\Controller;
-use App\Models\UserActionLog;
 use App\Repositories\UserActionLogRepository;
 use ManaPHP\Di\Attribute\Autowired;
 use ManaPHP\Http\Controller\Attribute\Authorize;
 use ManaPHP\Http\Router\Attribute\GetMapping;
 use ManaPHP\Http\Router\Attribute\RequestMapping;
 use ManaPHP\Mvc\View\Attribute\ViewGetMapping;
+use ManaPHP\Persistence\Page;
 use ManaPHP\Persistence\Restrictions;
 
 #[Authorize('user')]
@@ -35,10 +35,10 @@ class ActionLogController extends Controller
     #[ViewGetMapping]
     public function latestAction(int $page = 1, int $size = 10)
     {
-        return UserActionLog::select()
-            ->where(['user_id' => $this->identity->getId()])
-            ->where(Restrictions::of($this->request->all(), ['path', 'client_ip', 'created_time@=', 'tag']))
-            ->orderBy(['id' => SORT_DESC])
-            ->paginate($page, $size);
+        $restrictions = Restrictions::of($this->request->all(), ['path', 'client_ip', 'created_time@=', 'tag']);
+        $restrictions->eq('user_id', $this->identity->getId());
+
+        $orders = ['id' => SORT_DESC];
+        return $this->userActionLogRepository->paginate([], $restrictions, $orders, Page::of($page, $size));
     }
 }
--- app-user/app/Areas/User/Controllers/LoginLogController.php
+++ app-user/app/Areas/User/Controllers/LoginLogController.php
@@ -4,23 +4,30 @@ declare(strict_types=1);
 namespace App\Areas\User\Controllers;
 
 use App\Controllers\Controller;
-use App\Models\UserLoginLog;
+use App\Repositories\UserLoginLogRepository;
+use ManaPHP\Di\Attribute\Autowired;
 use ManaPHP\Http\Controller\Attribute\Authorize;
 use ManaPHP\Http\Router\Attribute\RequestMapping;
 use ManaPHP\Mvc\View\Attribute\ViewGetMapping;
+use ManaPHP\Persistence\Page;
 use ManaPHP\Persistence\Restrictions;
 
 #[RequestMapping('/user/login-log')]
 class LoginLogController extends Controller
 {
+    #[Autowired] protected UserLoginLogRepository $userLoginLogRepository;
+
     #[Authorize('user')]
     #[ViewGetMapping]
     public function latestAction(int $page = 1, int $size = 10)
     {
-        return UserLoginLog::select(['login_id', 'client_udid', 'user_agent', 'client_ip', 'created_time'])
-            ->where(Restrictions::of($this->request->all(), ['created_time@=']))
-            ->orderBy(['login_id' => SORT_DESC])
-            ->where(['user_id' => $this->identity->getId()])
-            ->paginate($page, $size);
+        $fields = ['login_id', 'client_udid', 'user_agent', 'client_ip', 'created_time'];
+
+        $restrictions = Restrictions::of($this->request->all(), ['created_time@=']);
+        $restrictions->eq('user_id', $this->identity->getId());
+
+        $orders = ['login_id' => SORT_DESC];
+
+        return $this->userLoginLogRepository->paginate($fields, $restrictions, $orders, Page::of($page, $size));
     }
 }
