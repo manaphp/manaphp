3991afea140006a8ed84dcffccf17425bd83fdeb
ManaPHP Mark <manaphp@qq.com>
2024-05-22 21:05:51 +0800

    \ManaPHP\Validating\Constraint\Attribute\Immutable

--- framework/Validating/Constraint/Attribute/Immutable.php
+++ framework/Validating/Constraint/Attribute/Immutable.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 namespace ManaPHP\Validating\Constraint\Attribute;
 
 use Attribute;
+use ManaPHP\Di\Attribute\Autowired;
 use ManaPHP\Exception\MisuseException;
 use ManaPHP\Persistence\Entity;
+use ManaPHP\Persistence\EntityMetadataInterface;
 use ManaPHP\Validating\AbstractConstraint;
 use ManaPHP\Validating\Validation;
 use function sprintf;
@@ -13,17 +15,27 @@ use function sprintf;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Immutable extends AbstractConstraint
 {
+    #[Autowired] protected EntityMetadataInterface $entityMetadata;
+
     public function validate(Validation $validation): bool
     {
-        /** @var Entity $source */
-        $source = $validation->source;
-        if (!$source instanceof Entity) {
-            throw new MisuseException(sprintf('%s is not a entity', $source::class));
+        if ($validation->value === null) {
+            return true;
         }
 
-        $field = $validation->field;
-        $snapshot = $source->getSnapshotData();
+        /** @var Entity $entity */
+        $entity = $validation->source;
+        $entityClass = $entity::class;
+        if (!$entity instanceof Entity) {
+            throw new MisuseException(sprintf('%s is not a entity', $entityClass));
+        }
 
-        return !isset($snapshot[$field]) || $snapshot[$field] === $validation->value;
+        $primaryKey = $this->entityMetadata->getPrimaryKey($entityClass);
+        if (!isset($entity->$primaryKey)) {
+            return true;
+        }
+
+        $repository = $this->entityMetadata->getRepository($entityClass);
+        return $validation->value === $repository->value([$primaryKey => $entity->$primaryKey], $validation->field);
     }
 }
