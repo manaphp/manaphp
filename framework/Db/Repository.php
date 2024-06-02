c7ac5ddb522beddf3759b336731658bdd32945d7
ManaPHP Mark <manaphp@qq.com>
2024-06-02 11:49:46 +0800

    \ManaPHP\Db\Repository \ManaPHP\Mongodb\Repository

--- framework/Db/Repository.php
+++ framework/Db/Repository.php
@@ -5,7 +5,12 @@ namespace ManaPHP\Db;
 
 use ManaPHP\Di\Attribute\Autowired;
 use ManaPHP\Persistence\AbstractRepository;
+use ManaPHP\Persistence\Entity;
 
+/**
+ * @template T of Entity
+ * @extends AbstractRepository<T>
+ */
 class Repository extends AbstractRepository
 {
     #[Autowired] protected EntityManagerInterface $entityManager;
