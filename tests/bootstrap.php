678363db9f49a7ca086e75ac7f9f3fdc40246cd2
ManaPHP Mark <manaphp@qq.com>
2025-11-25 09:19:32 +0800

    phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,24 +14,3 @@ if (file_exists($autoloadFile)) {
 } else {
     throw new RuntimeException('Composer autoload file not found. Please run "composer install" in app-admin directory.');
 }
-
-// Register autoloader for Tests namespace
-spl_autoload_register(function ($class) {
-    // Handle Tests namespace
-    if (strpos($class, 'Tests\\') === 0) {
-        // Remove 'Tests\' prefix (6 characters)
-        $relativePath = substr($class, 6);
-        // Convert namespace separators to directory separators
-        $relativePath = str_replace('\\', '/', $relativePath);
-        // Build full file path
-        $file = __DIR__ . '/' . $relativePath . '.php';
-        
-        if (file_exists($file)) {
-            require $file;
-            return true;
-        }
-    }
-    
-    return false;
-}, true, true);
-
